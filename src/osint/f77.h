dc6c9c230d6ce0320b5ca8c1f106a1f42bcc0c13
shiozaki <shiozaki@b6ac4546-25ea-4b55-bbba-8c74b0818de3>
2011-12-31 21:09:52 +0000

    removing deleted files from svn
    git-svn-id: file:///Users/shiozaki/svn/newint@188 b6ac4546-25ea-4b55-bbba-8c74b0818de3

--- src/osint/f77.h
+++
@@ -1,3 +0,0 @@
-extern "C" {
- void daxpy_(const int*, const double*, const double*, const int*, double*, const int*);
-};
--- src/pmp2/f77.h
+++
@@ -1,8 +0,0 @@
-#ifndef __src_pmp2_f77_h
-#define __src_pmp2_f77_h
-
-extern "C" {
- void start_up_slater_();
-};
-
-#endif
