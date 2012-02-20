c625c0db5d84945ffa61f5a7bf03a01b00309783
Toru Shiozaki <shiozaki@theochem.uni-stuttgart.de>
2012-02-20 01:04:53 +0100

    GPL headers added.

--- src/rysint/eribatch.cc
+++ src/rysint/eribatch.cc
@@ -1,7 +1,28 @@
 //
-// Author: Toru Shiozaki
-// Date  : April 2009
+// Newint - Parallel electron correlation program.
+// Filename: eribatch.cc
+// Copyright (C) 2009 Toru Shiozaki
 //
+// Author: Toru Shiozaki <shiozaki.toru@gmail.com>
+// Maintainer: Shiozaki group
+//
+// This file is part of the Newint package (to be renamed).
+//
+// The Newint package is free software; you can redistribute it and\/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The Newint package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the Newint package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
 #define PITWOHALF 17.493418327624862
 #define PIMHALF 0.564189583547756
 #define SQRTPI2 0.886226925452758013649083741671
