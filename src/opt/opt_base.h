eded404a85e1b60b9d20d6cc6f750fa4ba00603a
Toru Shiozaki <shiozaki@northwestern.edu>
2012-05-13 13:58:54 +0900

    Started to implement an optimization code.

---
+++ src/opt/opt_base.h
@@ -0,0 +1,42 @@
+//
+// Newint - Parallel electron correlation program.
+// Filename: opt_base.h
+// Copyright (C) 2012 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the Newint package (to be renamed).
+//
+// The Newint package is free software; you can redistribute it and\/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The Newint package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the Newint package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+
+#ifndef __SRC_OPT_OPT_BASE_H
+#define __SRC_OPT_OPT_BASE_H
+
+/// base class for optimization
+class Opt_base {
+  protected:
+
+  public:
+    Opt_base() {};
+    ~Opt_base() {};
+
+    void compute();
+
+};
+
+#endif
