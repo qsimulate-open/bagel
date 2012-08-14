cea6a92d803ac653048da31a924f3ceb9a610e00
Toru Shiozaki <shiozaki@northwestern.edu>
2012-08-14 18:13:18 -0500

    Minor cleanup.

--- src/osint/overlapbatch.cc
+++ src/osint/overlapbatch.cc
@@ -24,11 +24,19 @@
 //
 
 
-// compute overlap and kinetic integrals using Obara-Saika recursion formula
-
+#include <cassert>
+#include <iostream>
+#include <src/stackmem.h>
 #include <src/osint/overlapbatch.h>
+#include <src/rysint/hrrlist.h>
+#include <src/rysint/carsphlist.h>
+#include <src/rysint/macros.h>
+
 
 using namespace std;
+
+static HRRList hrr;
+extern StackMem* stack;
 
 OverlapBatch::OverlapBatch(const vector<std::shared_ptr<const Shell> >& _basis) 
  : OSInt(_basis) {
@@ -37,5 +45,48 @@ OverlapBatch::OverlapBatch(const vector<std::shared_ptr<const Shell> >& _basis)
 
 
 OverlapBatch::~OverlapBatch() {
+}
+
+
+void OverlapBatch::compute() {
+  double* stack_save = stack->get(0);
+
+  double* intermediate_p = stack->get(prim0_ * prim1_ * asize_);
+  perform_VRR(intermediate_p);
+
+  double* intermediate_c = stack->get(cont0_ * cont1_ * asize_);
+  fill(intermediate_c, intermediate_c + cont0_ * cont1_ * asize_, 0.0);
+  perform_contraction(asize_, intermediate_p, prim0_, prim1_, intermediate_c, 
+                      basisinfo_[0]->contractions(), basisinfo_[0]->contraction_ranges(), cont0_, 
+                      basisinfo_[1]->contractions(), basisinfo_[1]->contraction_ranges(), cont1_);
+
+  double* intermediate_fi = stack->get(cont0_ * cont1_ * asize_intermediate_);
+
+  if (basisinfo_[1]->angular_number() != 0) { 
+    const int hrr_index = basisinfo_[0]->angular_number() * ANG_HRR_END + basisinfo_[1]->angular_number();
+    hrr.hrrfunc_call(hrr_index, cont0_ * cont1_, intermediate_c, AB_, intermediate_fi);
+  } else {
+    const unsigned int array_size = cont0_ * cont1_ * asize_intermediate_;
+    copy(intermediate_c, intermediate_c+array_size, intermediate_fi);
+  }
+
+  if (spherical_) {
+    struct CarSphList carsphlist;
+    double* intermediate_i = stack->get(cont0_ * cont1_ * asize_final_);
+    const unsigned int carsph_index = basisinfo_[0]->angular_number() * ANG_HRR_END + basisinfo_[1]->angular_number();
+    const int nloops = cont0_ * cont1_;
+    carsphlist.carsphfunc_call(carsph_index, nloops, intermediate_fi, intermediate_i); 
+
+    const unsigned int sort_index = basisinfo_[1]->angular_number() * ANG_HRR_END + basisinfo_[0]->angular_number();
+    sort_.sortfunc_call(sort_index, data_, intermediate_i, cont1_, cont0_, 1, swap01_);
+    stack->release(cont0_ * cont1_ * asize_final_);
+  } else {
+    const unsigned int sort_index = basisinfo_[1]->angular_number() * ANG_HRR_END + basisinfo_[0]->angular_number();
+    sort_.sortfunc_call(sort_index, data_, intermediate_fi, cont1_, cont0_, 1, swap01_);
+  }
+
+  stack->release(prim0_ * prim1_ * asize_ + cont0_ * cont1_ * asize_ + cont0_ * cont1_ * asize_intermediate_);
+  assert(stack->get(0) == stack_save);
+
 }
 
