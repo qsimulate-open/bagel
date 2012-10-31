cfcf9e1614656db35c48c344e9c6d948d11b7132
Toru Shiozaki <shiozaki@northwestern.edu>
2012-10-31 14:14:04 -0500

    minor

--- src/osint/momentbatch.cc
+++ src/osint/momentbatch.cc
@@ -81,7 +81,6 @@ void MomentBatch::compute() {
   }
   stack_->release(prim0_*prim1_*asize_intermediate_*3, intermediate_p);
 
-// TODO not sure if this is needed
   if (swap01_) dscal_(size_alloc_, -1.0, data_, 1);
 
 }
