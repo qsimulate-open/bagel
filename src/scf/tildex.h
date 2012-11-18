164a21629c069e9b40160e1773c00ba13a600ebf
Toru Shiozaki <shiozaki@northwestern.edu>
2012-11-17 22:58:57 -0600

    minor

--- src/scf/tildex.h
+++ src/scf/tildex.h
@@ -36,7 +36,9 @@ class TildeX : public Matrix {
   protected:
 
   public:
-    TildeX(const std::shared_ptr<Overlap>, const double thresh);
+    TildeX(const std::shared_ptr<Overlap> olp, const double thresh) : Matrix(*olp)  {
+      this->inverse_half(thresh);
+    }
 
     TildeX& operator=(const Matrix1e& o) { std::copy(o.data(), o.data()+ndim_*mdim_, this->data()); return *this; };
 
