92bd6c94d7a861e440b1383ef91731ffc82080fb
Toru Shiozaki <shiozaki@northwestern.edu>
2012-12-26 10:18:27 -0600

    Revert "after looking at assembly codes for I decided to explicitly instantiate these template functions."
    This reverts commit c81ad92125d53e0fb626364ae3c39acac876b1cc.

--- src/rysint/func/vrr_driver_6_0_5_0.cc
+++
@@ -1,7 +0,0 @@
-#include <src/rysint/_vrr_drv.h>
-void bagel::VRR_Driver::vrr_driver_6_0_5_0(double* out, const double* const roots, const double* const weights, const double& coeff,
-    const std::array<double,3>& a, const std::array<double,3>& b, const std::array<double,3>& c, const std::array<double,3>& d,
-    const double* const p, const double* const q, const double& xp, const double& xq, 
-    const int* const amap, const int* const cmap, const int& asize_, double* const workx, double* const worky, double* const workz) {
-  bagel::vrr_driver<6,0,5,0,6>(out, roots, weights, coeff, a, b, c, d, p, q, xp, xq, amap, cmap, asize_, workx, worky, workz);
-}
