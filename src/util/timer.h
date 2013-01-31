75b6f10da33eb1561f402dc4bc606cfaa643ee3f
Toru Shiozaki <shiozaki@northwestern.edu>
2013-01-30 20:47:20 -0600

    fix

--- src/util/timer.h
+++ src/util/timer.h
@@ -61,7 +61,7 @@ class Timer {
         std::transform(title.begin(), title.end(), title.begin(), ::toupper);
         std::cout << "    * " << std::left << std::setw(39) << title << std::right << std::setw(10) << std::setprecision(2) << tick() << std::endl;
 #ifdef HAVE_MPI_H
-      } else if (level_ >= std::max(1, resources__->proc()->print_level())) {
+      } else if (level_ >= 1 && level_ <= resources__->proc()->print_level()) {
         const std::string indent(13+2*level_, ' ');
         const std::string mark = (level_ == 1 ? "o" : (level_ == 2 ? "*" : "-"));
         std::cout << indent << std::left << mark << " " << std::setw(35) << title << std::right << std::setw(13) << std::setprecision(2) << tick() << std::endl;
