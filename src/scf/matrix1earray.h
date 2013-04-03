d952bcdc18448a54d17e40181835dc6f99572949
Shane Parker <shane.parker@u.northwestern.edu>
2013-04-03 14:13:29 -0500

    Matrix1eArray no longer derives from Matrix1e. This should be safer. It will definitely not work with distributed matrices at the moment, since I had to comment out most of the MPI related stuff. All the tests pass, but I would still advise caution with it for the moment.

--- src/scf/matrix1earray.h
+++ src/scf/matrix1earray.h
@@ -39,9 +39,11 @@ namespace bagel {
 
 // specialized matrix for N component 1e integrals
 template <int N>
-class Matrix1eArray : public Matrix1e {
+class Matrix1eArray {
   protected:
     std::array<std::shared_ptr<Matrix>, N> matrices_;
+
+    std::shared_ptr<const Geometry> geom_;
 
     virtual void computebatch(const std::array<std::shared_ptr<const Shell>,2>&, const int, const int) = 0;
 
@@ -58,34 +60,35 @@ class Matrix1eArray : public Matrix1e {
     std::shared_ptr<const Matrix> operator[](const int i) const { return matrices_[i]; }
     constexpr const int nblocks() { return N; }
 
-    void fill_upper() override { for (int i = 0 ; i < N; ++i) matrices_[i]->fill_upper(); }
+    void fill_upper() { for (int i = 0 ; i < N; ++i) matrices_[i]->fill_upper(); }
 
     virtual void print(const std::string name = "") const;
+    virtual void init();
 
 };
 
 template <int N>
-Matrix1eArray<N>::Matrix1eArray(const std::shared_ptr<const Geometry> geom) : Matrix1e(geom) {
+Matrix1eArray<N>::Matrix1eArray(const std::shared_ptr<const Geometry> geom) : geom_(geom) {
   for(int i = 0; i < N; ++i) {
     matrices_[i] = std::shared_ptr<Matrix>(new Matrix(geom->nbasis(), geom->nbasis()));
-//  matrices_[i]->zero(); // done in Matrix_base
   }
 }
 
 
 template <int N>
-Matrix1eArray<N>::Matrix1eArray(const std::shared_ptr<const Geometry> geom, const int n, const int m) : Matrix1e(geom) {
+Matrix1eArray<N>::Matrix1eArray(const std::shared_ptr<const Geometry> geom, const int n, const int m) : geom_(geom) {
   for(int i = 0; i < N; ++i) {
     matrices_[i] = std::shared_ptr<Matrix>(new Matrix(n, m));
-//  matrices_[i]->zero(); // done in Matrix_base
   }
 }
 
 
 template <int N>
 Matrix1eArray<N>::Matrix1eArray(const Matrix1eArray& o) : Matrix1eArray(o.geom()) {
+  const int ndim = matrices_.front()->ndim();
+  const int mdim = matrices_.front()->mdim();
   for (int i = 0; i < N; ++i) {
-    copy_n(o.data(i)->data(), ndim_*mdim_, this->data(i)->data()); 
+    copy_n(o.data(i)->data(), ndim*mdim, this->data(i)->data()); 
   }
 }
 
@@ -97,6 +100,46 @@ void Matrix1eArray<N>::print(const std::string name) const {
     std::stringstream ss; ss << name << " " << j++;
     i->print(ss.str());
   }
+}
+
+template <int N>
+void Matrix1eArray<N>::init() {
+
+  // identical to Matrix1e::init()
+  // only lower half will be stored
+  // TODO rewrite. thread. parallel. distribute
+
+  auto o0 = geom_->offsets().begin();
+  int u = 0;
+  for (auto a0 = geom_->atoms().begin(); a0 != geom_->atoms().end(); ++a0, ++o0) {
+    // iatom1 = iatom1;
+    auto offset0 = o0->begin();
+    for (auto b0 = (*a0)->shells().begin(); b0 != (*a0)->shells().end(); ++b0, ++offset0) {
+      auto offset1 = o0->begin();
+      for (auto b1 = (*a0)->shells().begin(); b1 != (*a0)->shells().end(); ++b1, ++offset1) {
+        if (u++ % mpi__->size() == mpi__->rank()) {
+          std::array<std::shared_ptr<const Shell>,2> input = {{*b1, *b0}};
+          computebatch(input, *offset0, *offset1);
+        }   
+      }   
+    }   
+
+    auto o1 = o0+1;
+    for (auto a1 = a0+1; a1 != geom_->atoms().end(); ++a1, ++o1) {
+      auto offset0 = o0->begin();
+      for (auto b0 = (*a0)->shells().begin(); b0 != (*a0)->shells().end(); ++b0, ++offset0) {
+        auto offset1 = o1->begin();
+        for (auto b1 = (*a1)->shells().begin(); b1 != (*a1)->shells().end(); ++b1, ++offset1) {
+          if (u++ % mpi__->size() == mpi__->rank()) {
+            std::array<std::shared_ptr<const Shell>,2> input = {{*b1, *b0}};
+            computebatch(input, *offset0, *offset1);
+          }   
+        }   
+      }   
+    }   
+  }
+  //mpi__->allreduce(data_.get(), size());
+
 }
 
 }
