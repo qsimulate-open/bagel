7dcb4547ea8fa9e7ca71c0b795d12dfb70f34e55
Toru Shiozaki <shiozaki@northwestern.edu>
2013-07-05 11:50:37 -0500

    house keeping

--- src/rysint/interpolate/replace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-for FILE in *.f; do cp $FILE $FILE.bak; sed -e "s/e-/d-/g" $FILE.bak > $FILE; rm $FILE.bak; done
-for FILE in *.f; do cp $FILE $FILE.bak; sed -e "s/e+/d+/g" $FILE.bak > $FILE; rm $FILE.bak; done
