9434ce3b4f44dea5e603745733b9faf7593e27ef
Toru Shiozaki <shiozaki@northwestern.edu>
2013-08-02 23:16:04 -0500

    typo in the header (since long time ago)

--- src/fci/space.h
+++ src/fci/space.h
@@ -10,7 +10,7 @@
 //
 // This file is part of the BAGEL package.
 //
-// The BAGEL package is free software; you can redistribute it and\/or modify
+// The BAGEL package is free software; you can redistribute it and/or modify
 // it under the terms of the GNU Library General Public License as published by
 // the Free Software Foundation; either version 2, or (at your option)
 // any later version.
