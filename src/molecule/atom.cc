1d6210a76398ebb846a1a9b8d1e93d79bb8ebf7a
Toru Shiozaki <shiozaki@northwestern.edu>
2013-08-04 00:14:15 -0500

    minor fix so that one can copy&paste the output geometry

--- src/molecule/atom.cc
+++ src/molecule/atom.cc
@@ -324,7 +324,7 @@ void Atom::print_basis() const {
 void Atom::print() const {
   string tmp = name_;
   tmp[0] = ::toupper(tmp[0]);
-  cout << "  { \"atom\" : \"" << setw(2) << tmp << "\", \"xyz\" : [" << fixed << setprecision(6) <<
+  cout << "  { \"atom\" : \"" << tmp << "\", \"xyz\" : [" << fixed << setprecision(6) <<
       setw(14) << position_[0] << "," <<
       setw(14) << position_[1] << "," <<
       setw(14) << position_[2] << " ]";
