af170d4e21d6fccb6e42dec463209feea6293f37
Toru Shiozaki <shiozaki@northwestern.edu>
2013-08-11 12:37:08 -0500

    cherry pick 2979aa7598c0fd86ec223583786b5845cfbc7a8b

--- src/zfci/zfci.h
+++ src/zfci/zfci.h
@@ -26,11 +26,10 @@
 #ifndef __BAGEL_ZFCI_ZFCI_H
 #define __BAGEL_ZFCI_ZFCI_H
 
-#include <src/zfci/zdvec.h>
+#include <src/fci/dvec.h>
 #include <src/zfci/zmofile.h>
 #include <src/wfn/ciwfn.h>
 #include <src/math/zmatrix.h>
-#include <src/fci/civec.h>
 
 namespace bagel {
 
