dfdde341a8d6df87fcf3c51df973dace46978b45
Toru Shiozaki <shiozaki@northwestern.edu>
2013-09-29 11:26:23 -0500

    MOFile code quality

--- src/fci/mofile.cc
+++ src/fci/mofile.cc
@@ -36,7 +36,7 @@ using namespace bagel;
 
 
 MOFile::MOFile(const shared_ptr<const Reference> ref, const string method)
-: geom_(ref->geom()), ref_(ref), core_fock_(make_shared<Matrix>(geom_->nbasis(), geom_->nbasis())), coeff_(ref_->coeff()) {
+: geom_(ref->geom()), ref_(ref), coeff_(ref_->coeff()) {
 
   do_df_ = geom_->df().get();
   if (!do_df_) throw runtime_error("for the time being I gave up maintaining non-DF codes.");
@@ -46,7 +46,7 @@ MOFile::MOFile(const shared_ptr<const Reference> ref, const string method)
 
 
 MOFile::MOFile(const shared_ptr<const Reference> ref, const shared_ptr<const Coeff> c, const string method)
-: hz_(false), geom_(ref->geom()), ref_(ref), core_fock_(make_shared<Matrix>(geom_->nbasis(), geom_->nbasis())), coeff_(c) {
+: hz_(false), geom_(ref->geom()), ref_(ref), coeff_(c) {
 
   do_df_ = geom_->df().get();
   if (!do_df_) throw runtime_error("for the time being I gave up maintaining non-DF codes.");
@@ -55,22 +55,29 @@ MOFile::MOFile(const shared_ptr<const Reference> ref, const shared_ptr<const Coe
 }
 
 
-double MOFile::create_Jiiii(const int nstart, const int nfence) {
+void MOFile::init(const int nstart, const int nfence) {
 
   // first compute all the AO integrals in core
   nocc_ = nfence - nstart;
 
+  // core energy is set here
+  if (nstart != 0) {
+    shared_ptr<Matrix> den = coeff_->form_density_rhf(nstart);
+    core_fock_ = make_shared<Fock<1>>(geom_, ref_->hcore(), den, ref_->schwarz());
+    core_energy_ = (*den * (*ref_->hcore()+*core_fock_)).trace() * 0.5;
+  } else {
+    core_fock_ = ref_->hcore(); 
+    core_energy_ = 0.0;
+  }
+
   // one electron part
-  double core_energy;
-  shared_ptr<const Matrix> buf1e;
-  tie(buf1e, core_energy) = compute_mo1e(nstart, nfence);
+  shared_ptr<const Matrix> buf1e = compute_mo1e(nstart, nfence);
 
   // two electron part.
   // this fills mo2e_1ext_ and returns buf2e which is an ii/ii quantity
   shared_ptr<const Matrix> buf2e = compute_mo2e(nstart, nfence);
 
   compress(buf1e, buf2e);
-  return core_energy;
 }
 
 
@@ -130,25 +137,9 @@ void MOFile::update_1ext_ints(const shared_ptr<const Matrix>& coeff) {
 }
 
 
-tuple<shared_ptr<const Matrix>, double> Jop::compute_mo1e(const int nstart, const int nfence) {
-
-  const int ncore = nstart;
-  double core_energy = 0.0;
-
-  auto fock0 = make_shared<Matrix>(*ref_->hcore());
-  // if core fock operator is not the same as hcore...
-  if (nstart != 0) {
-    shared_ptr<Matrix> den = coeff_->form_density_rhf(ncore);
-    fock0 = make_shared<Fock<1>>(geom_, ref_->hcore(), den, ref_->schwarz());
-    core_energy = (*den * (*ref_->hcore()+*fock0)).trace() * 0.5;
-    *core_fock_ = *fock0;
-  }
-  fock0->fill_upper();
-
+shared_ptr<const Matrix> Jop::compute_mo1e(const int nstart, const int nfence) {
   shared_ptr<const Matrix> ocoeff = coeff_->slice(nstart, nfence);
-  auto mat = make_shared<const Matrix>(*ocoeff % *fock0 * *ocoeff);
-
-  return make_tuple(mat, core_energy);
+  return make_shared<const Matrix>(*ocoeff % *core_fock_ * *ocoeff);
 }
 
 
