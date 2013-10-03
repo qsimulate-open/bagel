81654a98b5fadb04293d96cdb3d1b2cc6705890f
Toru Shiozaki <shiozaki@northwestern.edu>
2013-10-02 19:23:43 -0500

    sorry forgot to add this file...

---
+++ src/zfci/zharrison_denom.cc
@@ -0,0 +1,87 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: zharrison_denom.cc
+// Copyright (C) 2013 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <iomanip>
+#include <stdexcept>
+#include <bitset>
+#include <src/zfci/zharrison.h>
+#include <src/util/combination.hpp>
+#include <src/util/constants.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace bagel;
+
+
+void ZHarrison::const_denom() {
+  Timer denom_t;
+  auto h = make_shared<ZMatrix>(norb_, 1);
+  auto jaa = make_shared<ZMatrix>(norb_, norb_);
+  auto jab = make_shared<ZMatrix>(norb_, norb_);
+  auto jbb = make_shared<ZMatrix>(norb_, norb_);
+  auto kaa = make_shared<ZMatrix>(norb_, norb_);
+  auto kbb = make_shared<ZMatrix>(norb_, norb_);
+
+  for (int i = 0; i != norb_; ++i) {
+    for (int j = 0; j <= i; ++j) {
+#if 0
+      jop[i*norb_+j] = jop[j*norb_+i] = 0.5*jop_->mo2e_hz(j, i, j, i);
+      kop[i*norb_+j] = kop[j*norb_+i] = 0.5*jop_->mo2e_hz(j, i, i, j);
+#endif
+    }
+    h->data(i) = jop_->mo1e(bitset<2>("00"), i,i);
+    assert(abs(h->data(i) - jop_->mo1e(bitset<2>("11"), i,i)) < 1.0e-8);
+  }
+  denom_t.tick_print("jop, kop");
+
+  denom_ = make_shared<RelDvec>(space_, 1);
+
+#if 0
+  double* iter = denom_->data();
+  TaskQueue<HZDenomTask> tasks(det()->stringa().size());
+  for (auto& ia : det()->stringa()) {
+    tasks.emplace_back(iter, ia, det_, jop.get(), kop.get(), h.get());
+    iter += det()->stringb().size();
+  }
+
+  tasks.compute();
+  denom_t.tick_print("denom");
+#endif
+}
+
+#if 0
+void HarrisonZarrabian::update(shared_ptr<const Coeff> c) {
+  // iiii file to be created (MO transformation).
+  // now jop_->mo1e() and jop_->mo2e() contains one and two body part of Hamiltonian
+  Timer timer;
+  jop_ = make_shared<Jop>(ref_, ncore_, ncore_+norb_, c, "HZ");
+
+  // right now full basis is used.
+  cout << "    * Integral transformation done. Elapsed time: " << setprecision(2) << timer.tick() << endl << endl;
+
+  const_denom();
+}
+#endif
