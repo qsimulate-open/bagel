2287e696d331bd9701e1e2a222cf8aa481f95c90
Toru Shiozaki <shiozaki@northwestern.edu>
2013-10-26 17:05:41 -0500

    stab at Rel CASSCF

---
+++ src/zcasscf/zcasscf.h
@@ -0,0 +1,48 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: zcasscf.h
+// Copyright (C) 2013 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef __SRC_ZCASSCF_ZCASSCF_H
+#define __SRC_ZCASSCF_ZCASSCF_H
+
+#include <src/wfn/method.h>
+
+namespace bagel {
+
+class ZCASSCF : public Method {
+  protected:
+  
+  public:
+    ZCASSCF(const std::shared_ptr<const PTree> idat, const std::shared_ptr<const Geometry> geom,
+            const std::shared_ptr<const Reference> = std::shared_ptr<const Reference>());
+
+    void compute();
+
+    // TODO
+    std::shared_ptr<const Reference> conv_to_ref() const override { return std::shared_ptr<const Reference>(); }
+};
+
+}
+
+#endif
