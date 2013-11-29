30503f7ec52d7d6e3a37e2f50a7321c4204258f6
Toru Shiozaki <shiozaki@northwestern.edu>
2013-11-29 07:27:12 -0600

    The bug fixed in Anh's branch was not pushed to master

--- src/math/sparsealgebra.cc
+++ src/math/sparsealgebra.cc
@@ -23,6 +23,7 @@
 // the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 
+#include <cstring>
 #include <src/math/algo.h>
 #include <bagel_config.h>
 #ifdef HAVE_MKL_H
@@ -38,7 +39,7 @@ void dcsrmm_(const char *transa, const int m, const int n, const int k, const do
 #ifdef HAVE_MKL_H
   mkl_dcsrmm_(transa, m, n, k, alpha, adata, acols, arind, b, ldb, beta, c, ldc);
 #else
-  if (transa != "N") throw logic_error("Only \"N\" case implemented for dcsrmm_");
+  if (strcmp(transa, "N") != 0) throw logic_error("Only \"N\" case implemented for dcsrmm_");
   for (int j = 0; j < n; ++j) {
     double* target = c + j*ldc;
     const double* source = b + j*ldb;
