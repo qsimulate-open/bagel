4ecfbc46268824bde272d02e2eb9f6d4e14c574d
Toru Shiozaki <shiozaki@northwestern.edu>
2013-11-29 02:39:41 -0600

    avoiding excessive use of "this"

--- src/integral/rys/eribatch_base.h
+++ src/integral/rys/eribatch_base.h
@@ -33,7 +33,7 @@
 namespace bagel {
 
 template <typename DataType>
- class ERIBatch_Base : public RysIntegral<DataType> {
+class ERIBatch_Base : public RysIntegral<DataType> {
 
   protected:
     // a hack for screening of three-center integrals
@@ -41,35 +41,35 @@ template <typename DataType>
 
     virtual void root_weight(const int ps) = 0;
 
-    // this sets this->T_ (+ U_), this->p_, this->q_, this->xp_, this->xq_, this->coeff_, and screening_size_
+    // this sets T_ (+ U_), p_, q_, xp_, xq_, coeff_, and screening_size_
     // for ERI evaulation. Other than that, we need to overload this function in a derived class
     void compute_ssss(const DataType integral_thresh) override {
-      const double ax = this->basisinfo_[0]->position(0);
-      const double ay = this->basisinfo_[0]->position(1);
-      const double az = this->basisinfo_[0]->position(2);
-      const double bx = this->basisinfo_[1]->position(0);
-      const double by = this->basisinfo_[1]->position(1);
-      const double bz = this->basisinfo_[1]->position(2);
-      const double cx = this->basisinfo_[2]->position(0);
-      const double cy = this->basisinfo_[2]->position(1);
-      const double cz = this->basisinfo_[2]->position(2);
-      const double dx = this->basisinfo_[3]->position(0);
-      const double dy = this->basisinfo_[3]->position(1);
-      const double dz = this->basisinfo_[3]->position(2);
+      const double ax = basisinfo_[0]->position(0);
+      const double ay = basisinfo_[0]->position(1);
+      const double az = basisinfo_[0]->position(2);
+      const double bx = basisinfo_[1]->position(0);
+      const double by = basisinfo_[1]->position(1);
+      const double bz = basisinfo_[1]->position(2);
+      const double cx = basisinfo_[2]->position(0);
+      const double cy = basisinfo_[2]->position(1);
+      const double cz = basisinfo_[2]->position(2);
+      const double dx = basisinfo_[3]->position(0);
+      const double dy = basisinfo_[3]->position(1);
+      const double dz = basisinfo_[3]->position(2);
 
-      const double* exp0 = this->basisinfo_[0]->exponents_pointer();
-      const double* exp1 = this->basisinfo_[1]->exponents_pointer();
-      const double* exp2 = this->basisinfo_[2]->exponents_pointer();
-      const double* exp3 = this->basisinfo_[3]->exponents_pointer();
-      const int nexp0 = this->basisinfo_[0]->num_primitive();
-      const int nexp1 = this->basisinfo_[1]->num_primitive();
-      const int nexp2 = this->basisinfo_[2]->num_primitive();
-      const int nexp3 = this->basisinfo_[3]->num_primitive();
+      const double* exp0 = basisinfo_[0]->exponents_pointer();
+      const double* exp1 = basisinfo_[1]->exponents_pointer();
+      const double* exp2 = basisinfo_[2]->exponents_pointer();
+      const double* exp3 = basisinfo_[3]->exponents_pointer();
+      const int nexp0 = basisinfo_[0]->num_primitive();
+      const int nexp1 = basisinfo_[1]->num_primitive();
+      const int nexp2 = basisinfo_[2]->num_primitive();
+      const int nexp3 = basisinfo_[3]->num_primitive();
 
-      DataType* const Ecd_save = this->stack_->template get<DataType>(this->prim2size_*this->prim3size_);
-      DataType* const qx_save = this->stack_->template get<DataType>(this->prim2size_*this->prim3size_);
-      DataType* const qy_save = this->stack_->template get<DataType>(this->prim2size_*this->prim3size_);
-      DataType* const qz_save = this->stack_->template get<DataType>(this->prim2size_*this->prim3size_);
+      DataType* const Ecd_save = stack_->template get<DataType>(prim2size_*prim3size_);
+      DataType* const qx_save = stack_->template get<DataType>(prim2size_*prim3size_);
+      DataType* const qy_save = stack_->template get<DataType>(prim2size_*prim3size_);
+      DataType* const qz_save = stack_->template get<DataType>(prim2size_*prim3size_);
 
       const double minexp0 = *std::min_element(exp0, exp0+nexp0);
       const double minexp1 = *std::min_element(exp1, exp1+nexp1);
@@ -81,9 +81,9 @@ template <typename DataType>
       const double min_cdp = minexp2 * minexp3;
 
       // minimum distance between two lines (AB and CD)
-      const double x_ab_cd = this->AB_[1] * this->CD_[2] - this->AB_[2] * this->CD_[1];
-      const double y_ab_cd = this->AB_[2] * this->CD_[0] - this->AB_[0] * this->CD_[2];
-      const double z_ab_cd = this->AB_[0] * this->CD_[1] - this->AB_[1] * this->CD_[0];
+      const double x_ab_cd = AB_[1] * CD_[2] - AB_[2] * CD_[1];
+      const double y_ab_cd = AB_[2] * CD_[0] - AB_[0] * CD_[2];
+      const double z_ab_cd = AB_[0] * CD_[1] - AB_[1] * CD_[0];
       const double x_n_ac = x_ab_cd * (ax - cx);
       const double y_n_ac = y_ab_cd * (ay - cy);
       const double z_n_ac = z_ab_cd * (az - cz);
@@ -91,11 +91,11 @@ template <typename DataType>
       const double norm_ab_cd_sq = x_ab_cd * x_ab_cd + y_ab_cd * y_ab_cd + z_ab_cd * z_ab_cd;
       const double min_pq_sq = norm_ab_cd_sq == 0.0 ? 0.0 : innerproduct * innerproduct / norm_ab_cd_sq;
 
-      const double r01_sq = this->AB_[0] * this->AB_[0] + this->AB_[1] * this->AB_[1] + this->AB_[2] * this->AB_[2];
-      const double r23_sq = this->CD_[0] * this->CD_[0] + this->CD_[1] * this->CD_[1] + this->CD_[2] * this->CD_[2];
+      const double r01_sq = AB_[0] * AB_[0] + AB_[1] * AB_[1] + AB_[2] * AB_[2];
+      const double r23_sq = CD_[0] * CD_[0] + CD_[1] * CD_[1] + CD_[2] * CD_[2];
 
       unsigned int tuple_length = 0u;
-      DataType* const tuple_field = this->stack_->template get<DataType>(nexp2*nexp3*3);
+      DataType* const tuple_field = stack_->template get<DataType>(nexp2*nexp3*3);
       int* tuple_index = (int*)(tuple_field+nexp2*nexp3*2);
       {
         const double cxp_min = minexp0 + minexp1;
@@ -140,8 +140,8 @@ template <typename DataType>
 
       int index = 0;
       int index01 = 0;
-      std::fill_n(this->T_, this->primsize_, -1.0);
-      this->screening_size_ = 0;
+      std::fill_n(T_, primsize_, -1.0);
+      screening_size_ = 0;
 
       const double cxq_min = minexp2 + minexp3;
       const double cxq_inv_min = 1.0 / cxq_min;
@@ -170,7 +170,7 @@ template <typename DataType>
             if (std::abs(ssss) < std::abs(integral_thresh)) continue;
           }
 
-          const int index_base = this->prim2size_ * this->prim3size_ * index01;
+          const int index_base = prim2size_ * prim3size_ * index01;
 
           for (unsigned int i = 0; i != tuple_length; ++i) {
             const int index23 = tuple_index[i];
@@ -178,80 +178,80 @@ template <typename DataType>
             const DataType exp2value = tuple_field[2*i];
             const DataType exp3value = tuple_field[2*i+1];
             const DataType cxq = exp2value + exp3value;
-            this->xp_[index] = cxp;
-            this->xq_[index] = cxq;
+            xp_[index] = cxp;
+            xq_[index] = cxq;
             const DataType cxpxq = cxp * cxq;
             const DataType onepqp_q = 1.0 / (std::sqrt(cxp + cxq) * cxpxq);
-            this->coeff_[index] = Ecd_save[index23] * coeff_half * onepqp_q;
+            coeff_[index] = Ecd_save[index23] * coeff_half * onepqp_q;
             const DataType rho = cxpxq / (cxp + cxq);
             const DataType xpq = qx_save[index23] - px;
             const DataType ypq = qy_save[index23] - py;
             const DataType zpq = qz_save[index23] - pz;
             const DataType T = rho * (xpq * xpq + ypq * ypq + zpq * zpq);
             const int index3 = index * 3;
-            this->p_[index3] = px;
-            this->p_[index3 + 1] = py;
-            this->p_[index3 + 2] = pz;
-            this->q_[index3] =     qx_save[index23];
-            this->q_[index3 + 1] = qy_save[index23];
-            this->q_[index3 + 2] = qz_save[index23];
-            this->T_[index] = T;
-            this->screening_[this->screening_size_] = index;
-            ++this->screening_size_;
+            p_[index3] = px;
+            p_[index3 + 1] = py;
+            p_[index3 + 2] = pz;
+            q_[index3] =     qx_save[index23];
+            q_[index3 + 1] = qy_save[index23];
+            q_[index3 + 2] = qz_save[index23];
+            T_[index] = T;
+            screening_[screening_size_] = index;
+            ++screening_size_;
           }
         }
       }
-      this->stack_->release(nexp2*nexp3*3, tuple_field);
-      this->stack_->release(this->prim2size_*this->prim3size_, qz_save);
-      this->stack_->release(this->prim2size_*this->prim3size_, qy_save);
-      this->stack_->release(this->prim2size_*this->prim3size_, qx_save);
-      this->stack_->release(this->prim2size_*this->prim3size_, Ecd_save);
+      stack_->release(nexp2*nexp3*3, tuple_field);
+      stack_->release(prim2size_*prim3size_, qz_save);
+      stack_->release(prim2size_*prim3size_, qy_save);
+      stack_->release(prim2size_*prim3size_, qx_save);
+      stack_->release(prim2size_*prim3size_, Ecd_save);
     }
 
     void allocate_data(const int asize_final, const int csize_final, const int asize_final_sph, const int csize_final_sph) override {
-      this->size_final_ = asize_final_sph * csize_final_sph * this->contsize_;
-      if (this->deriv_rank_ == 0) {
-        const unsigned int size_start = this->asize_ * this->csize_ * this->primsize_;
-        const unsigned int size_intermediate = asize_final * this->csize_ * this->contsize_;
-        const unsigned int size_intermediate2 = asize_final_sph * csize_final * this->contsize_;
-        this->size_block_ = std::max(size_start, std::max(size_intermediate, size_intermediate2));
-        this->size_alloc_ = this->size_block_;
+      size_final_ = asize_final_sph * csize_final_sph * contsize_;
+      if (deriv_rank_ == 0) {
+        const unsigned int size_start = asize_ * csize_ * primsize_;
+        const unsigned int size_intermediate = asize_final * csize_ * contsize_;
+        const unsigned int size_intermediate2 = asize_final_sph * csize_final * contsize_;
+        size_block_ = std::max(size_start, std::max(size_intermediate, size_intermediate2));
+        size_alloc_ = size_block_;
 
         // if this is a two-electron Breit integral
-        if (this->breit_)
-          this->size_alloc_ = 6 * this->size_block_;
+        if (breit_)
+          size_alloc_ = 6 * size_block_;
 
-        this->stack_save_ = this->stack_->get(this->size_alloc_);
-        this->stack_save2_ = nullptr;
+        stack_save_ = stack_->get(size_alloc_);
+        stack_save2_ = nullptr;
 
         // if Slater/Yukawa integrals
-        if (this->tenno_)
-          this->stack_save2_ = this->stack_->get(this->size_alloc_);
+        if (tenno_)
+          stack_save2_ = stack_->get(size_alloc_);
 
       // derivative integrals
-      } else if (this->deriv_rank_ == 1) {
-        this->size_block_ = asize_final * csize_final * this->primsize_;
+      } else if (deriv_rank_ == 1) {
+        size_block_ = asize_final * csize_final * primsize_;
         // if this is a two-electron gradient integral
         if (dynamic_cast<ERIBatch_Base*>(this)) {
-          this->size_alloc_ = 12 * this->size_block_;
+          size_alloc_ = 12 * size_block_;
         } else {
           throw std::logic_error("something is strange in ERIBatch_Base::allocate_data");
         }
-        this->stack_save_ = this->stack_->get(this->size_alloc_);
-        this->stack_save2_ = nullptr;
+        stack_save_ = stack_->get(size_alloc_);
+        stack_save2_ = nullptr;
       }
-      this->data_ = this->stack_save_;
-      this->data2_ = this->stack_save2_;
+      data_ = stack_save_;
+      data2_ = stack_save2_;
     }
 
   public:
 
     ERIBatch_Base(const std::array<std::shared_ptr<const Shell>,4>& o, const double max_density, const int deriv, const int breit = 0,
-                 std::shared_ptr<StackMem> stack = std::shared_ptr<StackMem>()) : RysIntegral<DataType>(o, stack) {
+                  std::shared_ptr<StackMem> stack = std::shared_ptr<StackMem>()) : RysIntegral<DataType>(o, stack) {
 
-      this->breit_ = breit;
+      breit_ = breit;
 
-      this->deriv_rank_ = deriv;
+      deriv_rank_ = deriv;
 
       // determines if we want to swap shells
       this->set_swap_info(true);
@@ -259,7 +259,7 @@ template <typename DataType>
       // stores AB and CD
       this->set_ab_cd();
 
-      // set this->primsize_ and this->contsize_, as well as relevant members
+      // set primsize_ and contsize_, as well as relevant members
       this->set_prim_contsizes();
 
       // sets angular info
@@ -268,12 +268,44 @@ template <typename DataType>
 
       // allocate
       allocate_data(asize_final, csize_final, asize_final_sph, csize_final_sph);
-      this->allocate_arrays(this->primsize_);
+      this->allocate_arrays(primsize_);
 
       const DataType integral_thresh = (max_density != 0.0) ? (PRIM_SCREEN_THRESH / max_density) : 0.0;
       compute_ssss(integral_thresh);
     }
- };
+
+  public:
+    using RysIntegral<DataType>::basisinfo_;
+    using RysIntegral<DataType>::stack_;
+    using RysIntegral<DataType>::stack_save_;
+    using RysIntegral<DataType>::stack_save2_;
+    using RysIntegral<DataType>::AB_;
+    using RysIntegral<DataType>::CD_;
+    using RysIntegral<DataType>::contsize_;
+    using RysIntegral<DataType>::asize_;
+    using RysIntegral<DataType>::csize_;
+    using RysIntegral<DataType>::primsize_;
+    using RysIntegral<DataType>::prim0size_;
+    using RysIntegral<DataType>::prim1size_;
+    using RysIntegral<DataType>::prim2size_;
+    using RysIntegral<DataType>::prim3size_;
+    using RysIntegral<DataType>::screening_;
+    using RysIntegral<DataType>::screening_size_;
+    using RysIntegral<DataType>::T_;
+    using RysIntegral<DataType>::p_;
+    using RysIntegral<DataType>::q_;
+    using RysIntegral<DataType>::xp_;
+    using RysIntegral<DataType>::xq_;
+    using RysIntegral<DataType>::size_block_;
+    using RysIntegral<DataType>::size_alloc_;
+    using RysIntegral<DataType>::size_final_;
+    using RysIntegral<DataType>::coeff_;
+    using RysIntegral<DataType>::deriv_rank_;
+    using RysIntegral<DataType>::tenno_;
+    using RysIntegral<DataType>::breit_;
+    using RysIntegral<DataType>::data_;
+    using RysIntegral<DataType>::data2_;
+};
 
 
 using ERIBatch_base = ERIBatch_Base<double>;
