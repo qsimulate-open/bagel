6ee56e1ad9f2c0a1425dd40759e748e37d7537ac
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2013-11-30 15:40:47 -0600

    Deactivate integral screening for London orbitals

--- src/integral/comprys/complexeribatch.cc
+++ src/integral/comprys/complexeribatch.cc
@@ -31,7 +31,12 @@ using namespace std;
 using namespace bagel;
 
 ComplexERIBatch::ComplexERIBatch(const array<shared_ptr<const Shell>,4>& _info, const double max_density, const std::complex<double> dummy, const bool dum,
-                   shared_ptr<StackMem> stack)  :  ERIBatch_Base(_info, max_density, 0, 0, stack) {
+                   shared_ptr<StackMem> stack)  :  ERIBatch_Base(_info, 0.0, 0, 0, stack) {
+
+// TODO Integral screening has not yet been implemented for London orbitals.  If we want to use integral screening,
+// then the second argument to the constructor of ERIBatch_Base should be max_density.  Since we currently pass
+// 0.0 as an argument, it should compute every integral.
+
 
 #ifdef LIBINT_INTERFACE
   assert(false);
@@ -39,6 +44,7 @@ ComplexERIBatch::ComplexERIBatch(const array<shared_ptr<const Shell>,4>& _info, 
 
 //  root_weight(this->primsize_);
 }
+
 
 void ComplexERIBatch::root_weight(const int ps) {
   if (breit_ == 0) {
@@ -49,9 +55,23 @@ void ComplexERIBatch::root_weight(const int ps) {
   }
 }
 
-void ComplexERIBatch::compute_ssss(const complex<double> integral_thresh) {
-  assert(0);
-  throw std::runtime_error("compute_ssss not yet implemented for London orbitals");
+
+// TODO For efficiency's sake, it's probably best to find a way to avoid repeatedly running basisinfo_[i]->vector_potential(j) each time we want to get a P or Q value
+inline std::complex<double> ComplexERIBatch::get_PQ (const double coord1, const double coord2, const double exp1, const double exp2, const double one12, const int center1, const int dim) {
+  const double Areal = coord1*exp1;
+  const double Breal = coord2*exp2;
+  const double Aimag = -0.5*basisinfo_[center1]->vector_potential(dim);
+  const double Bimag = -0.5*basisinfo_[center1+1]->vector_potential(dim);
+  const std::complex<double> num ( (Areal + Breal) , (Aimag + Bimag) );
+  return ( num * one12 );
 }
 
+// no screening for London orbitals at this point
+//void ComplexERIBatch::screen_CD() {}
+
+// no screening for London orbitals at this point
+//void ComplexERIBatch::screen_AB() {}
+
+
+//void ComplexERIBatch::save_ssss() {}
 
