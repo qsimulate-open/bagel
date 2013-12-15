36644c2ec6a840695830e89d9c99287e10a35211
Toru Shiozaki <shiozaki@northwestern.edu>
2013-12-14 22:41:37 -0600

    oops sorry

--- src/smith/task.h
+++ src/smith/task.h
@@ -71,7 +71,7 @@ class Task : public std::enable_shared_from_this<Task<T>> {
 
     bool done() const { return done_; }
 
-    bool ready() const { return !done_ && std::all_of(depends_.begin(), depends_.end(), [](std::shared_ptr<Task<T>>& o) { return o->done(); }); }
+    bool ready() const { return !done_ && std::all_of(depend_.begin(), depend_.end(), [](std::shared_ptr<Task<T>>& o) { return o->done(); }); }
 
     virtual double energy() const { return 0.0; }
     virtual double dedci() const { return 0.0; }
