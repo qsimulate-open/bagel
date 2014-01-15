qsimulate-open/bagel

commit 6af07d51eff180a379ac73aa568b1327f0f74039

KnowlesHandy form_sigma now uses the Matrix class

src/fci/knowles_compute.cc
@@ -36,14 +36,15 @@ shared_ptr<Dvec> KnowlesHandy::form_sigma(shared_ptr<const Dvec> ccvec, shared_p
                      const vector<int>& conv) const { // d and e are scratch area for D and E intermediates
 
   const int ij = nij();
+  const size_t lenab = ccvec->det()->lena() * ccvec->det()->lenb();
 
   const int nstate = ccvec->ij();
   auto sigmavec = make_shared<Dvec>(ccvec->det(), nstate);
   sigmavec->zero();
 
   // we need two vectors for intermediate quantities
-  auto d = make_shared<Dvec>(ccvec->det(), ij);
-  auto e = make_shared<Dvec>(ccvec->det(), ij);
+  auto d = make_shared<Matrix>(lenab, ij);
+  auto e = make_shared<Matrix>(lenab, ij);
 
 
   for (int istate = 0; istate != nstate; ++istate) {
@@ -88,14 +89,13 @@ shared_ptr<Dvec> KnowlesHandy::form_sigma(shared_ptr<const Dvec> ccvec, shared_p
   return sigmavec;
 }
 
-// The first two are a part of Base because they are needed in the RDM parts
-void FCI::sigma_2a1(shared_ptr<const Civec> cc, shared_ptr<Dvec> d) const {
+void KnowlesHandy::sigma_2a1(shared_ptr<const Civec> cc, shared_ptr<Matrix> d) const {
   assert(d->det() == cc->det());
-  const int lb = d->lenb();
-  const int ij = d->ij();
+  const int lb = cc->lenb();
+  const int ij = nij();
   const double* const source_base = cc->data();
   for (int ip = 0; ip != ij; ++ip) {
-    double* const target_base = d->data(ip)->data();
+    double* const target_base = d->element_ptr(0, ip);
     for (auto& iter : cc->det()->phia(ip)) {
       const double sign = static_cast<double>(iter.sign);
       double* const target_array = target_base + iter.source*lb;
@@ -104,14 +104,15 @@ void FCI::sigma_2a1(shared_ptr<const Civec> cc, shared_ptr<Dvec> d) const {
   }
 }
 
-void FCI::sigma_2a2(shared_ptr<const Civec> cc, shared_ptr<Dvec> d) const {
+void KnowlesHandy::sigma_2a2(shared_ptr<const Civec> cc, shared_ptr<Matrix> d) const {
   assert(d->det() == cc->det());
-  const int la = d->lena();
-  const int ij = d->ij();
+  const int la = cc->lena();
+  const int lb = cc->lenb();
+  const int ij = nij();
   for (int i = 0; i < la; ++i) {
     const double* const source_array0 = cc->element_ptr(0, i);
     for (int ip = 0; ip != ij; ++ip) {
-      double* const target_array0 = d->data(ip)->element_ptr(0, i);
+      double* const target_array0 = d->element_ptr(i*lb, ip);
       for (auto& iter : cc->det()->phib(ip)) {
         const double sign = static_cast<double>(iter.sign);
         target_array0[iter.source] += sign * source_array0[iter.target];
@@ -133,12 +134,12 @@ void KnowlesHandy::sigma_1(shared_ptr<const Civec> cc, shared_ptr<Civec> sigma, 
   }
 }
 
-void KnowlesHandy::sigma_2c1(shared_ptr<Civec> sigma, shared_ptr<const Dvec> e) const {
-  const int lb = e->lenb();
-  const int ij = e->ij();
+void KnowlesHandy::sigma_2c1(shared_ptr<Civec> sigma, shared_ptr<const Matrix> e) const {
+  const int lb = sigma->lenb();
+  const int ij = nij();
   for (int ip = 0; ip != ij; ++ip) {
-    const double* const source_base = e->data(ip)->data();
-    for (auto& iter : e->det()->phia(ip)) {
+    const double* const source_base = e->element_ptr(0, ip);
+    for (auto& iter : sigma->det()->phia(ip)) {
       const double sign = static_cast<double>(iter.sign);
       double* const target_array = sigma->element_ptr(0, iter.target);
       daxpy_(lb, sign, source_base + lb*iter.source, 1, target_array, 1);
@@ -146,14 +147,15 @@ void KnowlesHandy::sigma_2c1(shared_ptr<Civec> sigma, shared_ptr<const Dvec> e) 
   }
 }
 
-void KnowlesHandy::sigma_2c2(shared_ptr<Civec> sigma, shared_ptr<const Dvec> e) const {
-  const int la = e->lena();
-  const int ij = e->ij();
+void KnowlesHandy::sigma_2c2(shared_ptr<Civec> sigma, shared_ptr<const Matrix> e) const {
+  const int la = sigma->lena();
+  const int lb = sigma->lenb();
+  const int ij = nij();
   for (int i = 0; i < la; ++i) {
     double* const target_array0 = sigma->element_ptr(0, i);
     for (int ip = 0; ip != ij; ++ip) {
-      const double* const source_array0 = e->data(ip)->element_ptr(0, i);
-      for (auto& iter : e->det()->phib(ip)) {
+      const double* const source_array0 = e->element_ptr(i*lb, ip);
+      for (auto& iter : sigma->det()->phib(ip)) {
         const double sign = static_cast<double>(iter.sign);
         target_array0[iter.target] += sign * source_array0[iter.source];
       }
@@ -179,12 +181,9 @@ void KnowlesHandy::sigma_3(shared_ptr<const Civec> cc, shared_ptr<Civec> sigma, 
   }
 }
 
-void KnowlesHandy::sigma_2b(shared_ptr<Dvec> d, shared_ptr<Dvec> e, shared_ptr<const MOFile> jop) const {
-  const int la = d->lena();
-  const int lb = d->lenb();
-  const int ij = d->ij();
-  const int lenab = la*lb;
+void KnowlesHandy::sigma_2b(shared_ptr<Matrix> d, shared_ptr<Matrix> e, shared_ptr<const MOFile> jop) const {
+  const int ij = d->mdim();
+  const int lenab = d->ndim();
   dgemm_("n", "n", lenab, ij, ij, 0.5, d->data(), lenab, jop->mo2e_ptr(), ij,
                                   0.0, e->data(), lenab);
 }
-

src/fci/fci.h
@@ -173,8 +173,8 @@ class FCI : public Method {
     std::shared_ptr<Dvec> civectors() const { return cc_; }
 
     // These are needed for the RDM stuff, apparently
-    void sigma_2a1(std::shared_ptr<const Civec> cc, std::shared_ptr<Dvec> d) const;
-    void sigma_2a2(std::shared_ptr<const Civec> cc, std::shared_ptr<Dvec> d) const;
+    void displacements_alpha(std::shared_ptr<const Civec> cc, std::shared_ptr<Dvec> d) const;
+    void displacements_beta(std::shared_ptr<const Civec> cc, std::shared_ptr<Dvec> d) const;
 
     std::shared_ptr<const CIWfn> conv_to_ciwfn() const;
     std::shared_ptr<const Reference> conv_to_ref() const override { return std::shared_ptr<const Reference>(); }

src/fci/fci_rdm.cc
@@ -32,6 +32,38 @@
 using namespace std;
 using namespace bagel;
 
+void FCI::displacements_alpha(shared_ptr<const Civec> cc, shared_ptr<Dvec> d) const {
+  assert(d->det() == cc->det());
+  const int lb = cc->lenb();
+  const int ij = d->ij();
+  const double* const source_base = cc->data();
+  for (int ip = 0; ip != ij; ++ip) {
+    double* const target_base = d->data(ip)->data();
+    for (auto& iter : cc->det()->phia(ip)) {
+      const double sign = static_cast<double>(iter.sign);
+      double* const target_array = target_base + iter.source*lb;
+      daxpy_(lb, sign, source_base + iter.target*lb, 1, target_array, 1); 
+    }   
+  }
+}
+
+void FCI::displacements_beta(shared_ptr<const Civec> cc, shared_ptr<Dvec> d) const {
+  assert(d->det() == cc->det());
+  const int la = cc->lena();
+  const int lb = cc->lenb();
+  const int ij = d->ij();
+  for (int i = 0; i < la; ++i) {
+    const double* const source_array0 = cc->element_ptr(0, i); 
+    for (int ip = 0; ip != ij; ++ip) {
+      double* const target_array0 = d->data(ip)->element_ptr(0,i);
+      for (auto& iter : cc->det()->phib(ip)) {
+        const double sign = static_cast<double>(iter.sign);
+        target_array0[iter.source] += sign * source_array0[iter.target];
+      }   
+    }   
+  }
+}
+
 void FCI::compute_rdm12() {
   // Needs initialization here because we use daxpy.
   // For nstate_ == 1, rdm1_av_ = rdm1_[0].
@@ -103,8 +135,8 @@ shared_ptr<Dvec> FCI::rdm1deriv() const {
 
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   return dbra;
 }
@@ -119,8 +151,8 @@ shared_ptr<Dvec> FCI::rdm2deriv() const {
   // make  <I|E_ij|0>
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   // second make <J|E_kl|I><I|E_ij|0> - delta_li <J|E_kj|0>
   auto ebra = make_shared<Dvec>(cbra->det(), norb_*norb_*norb_*norb_);
@@ -131,8 +163,8 @@ shared_ptr<Dvec> FCI::rdm2deriv() const {
     const int j = ij/norb_;
     const int i = ij-j*norb_;
     tmp->zero();
-    sigma_2a1(*iter, tmp);
-    sigma_2a2(*iter, tmp);
+    displacements_alpha(*iter, tmp);
+    displacements_beta(*iter, tmp);
     int kl = 0;
     for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijkl, ++kl) {
       *ebra->data(ijkl) = **t;
@@ -153,8 +185,8 @@ shared_ptr<Dvec> FCI::rdm3deriv() const {
   // first make <I|i+j|0>
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   // second make <J|k+i+jl|0> = <J|k+l|I><I|i+j|0> - delta_li <J|k+j|0>
   auto ebra = make_shared<Dvec>(cbra->det(), norb_*norb_*norb_*norb_);
@@ -166,8 +198,8 @@ shared_ptr<Dvec> FCI::rdm3deriv() const {
       const int j = ij/norb_;
       const int i = ij-j*norb_;
       tmp->zero();
-      sigma_2a1(*iter, tmp);
-      sigma_2a2(*iter, tmp);
+      displacements_alpha(*iter, tmp);
+      displacements_beta(*iter, tmp);
       int kl = 0;
       for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijkl, ++kl) {
         *ebra->data(ijkl) = **t;
@@ -190,8 +222,8 @@ shared_ptr<Dvec> FCI::rdm3deriv() const {
       const int l = ijkl/norb_-i*norb_-j*norb_*norb_;
       const int k = ijkl-l*norb_-i*norb_*norb_-j*norb_*norb_*norb_;
       tmp->zero();
-      sigma_2a1(*iter, tmp);
-      sigma_2a2(*iter, tmp);
+      displacements_alpha(*iter, tmp);
+      displacements_beta(*iter, tmp);
       int mn = 0;
       for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijklmn, ++mn) {
         const int n = mn/norb_;
@@ -215,8 +247,8 @@ shared_ptr<Dvec> FCI::rdm4deriv() const {
   // first make <I|i+j|0>
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   // second make <J|k+i+jl|0> = <J|k+l|I><I|i+j|0> - delta_li <J|k+j|0>
   auto ebra = make_shared<Dvec>(cbra->det(), norb_*norb_*norb_*norb_);
@@ -228,8 +260,8 @@ shared_ptr<Dvec> FCI::rdm4deriv() const {
       const int j = ij/norb_;
       const int i = ij-j*norb_;
       tmp->zero();
-      sigma_2a1(*iter, tmp);
-      sigma_2a2(*iter, tmp);
+      displacements_alpha(*iter, tmp);
+      displacements_beta(*iter, tmp);
       int kl = 0;
       for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijkl, ++kl) {
         *ebra->data(ijkl) = **t;
@@ -252,8 +284,8 @@ shared_ptr<Dvec> FCI::rdm4deriv() const {
       const int l = ijkl/norb_-i*norb_-j*norb_*norb_;
       const int k = ijkl-l*norb_-i*norb_*norb_-j*norb_*norb_*norb_;
       tmp->zero();
-      sigma_2a1(*iter, tmp);
-      sigma_2a2(*iter, tmp);
+      displacements_alpha(*iter, tmp);
+      displacements_beta(*iter, tmp);
       int mn = 0;
       for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijklmn, ++mn) {
         const int n = mn/norb_;
@@ -279,8 +311,8 @@ shared_ptr<Dvec> FCI::rdm4deriv() const {
       const int n = ijklmn/norb_-k*norb_-l*norb_*norb_-i*norb_*norb_*norb_-j*norb_*norb_*norb_*norb_;
       const int m = ijklmn-n*norb_-k*norb_*norb_-l*norb_*norb_*norb_-i*norb_*norb_*norb_*norb_-j*norb_*norb_*norb_*norb_*norb_;
       tmp->zero();
-      sigma_2a1(*iter, tmp);
-      sigma_2a2(*iter, tmp);
+      displacements_alpha(*iter, tmp);
+      displacements_beta(*iter, tmp);
       int op = 0;
       for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijklmnop, ++op) {
         const int p = op/norb_;
@@ -303,16 +335,16 @@ tuple<shared_ptr<RDM<1>>, shared_ptr<RDM<2>>>
   // since we consider here number conserving operators...
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   shared_ptr<Dvec> dket;
   // if bra and ket vectors are different, we need to form Sigma for ket as well.
   if (cbra != cket) {
     dket = make_shared<Dvec>(cket->det(), norb_*norb_);
     dket->zero();
-    sigma_2a1(cket, dket);
-    sigma_2a2(cket, dket);
+    displacements_alpha(cket, dket);
+    displacements_beta(cket, dket);
   } else {
     dket = dbra;
   }
@@ -386,8 +418,8 @@ tuple<shared_ptr<RDM<3>>, shared_ptr<RDM<4>>> FCI::compute_rdm34(const int ist) 
   // first make <I|E_ij|0>
   auto dbra = make_shared<Dvec>(cbra->det(), norb_*norb_);
   dbra->zero();
-  sigma_2a1(cbra, dbra);
-  sigma_2a2(cbra, dbra);
+  displacements_alpha(cbra, dbra);
+  displacements_beta(cbra, dbra);
 
   // second make <J|E_kl|I><I|E_ij|0> - delta_li <J|E_kj|0>
   auto ebra = make_shared<Dvec>(cbra->det(), norb_*norb_*norb_*norb_);
@@ -398,8 +430,8 @@ tuple<shared_ptr<RDM<3>>, shared_ptr<RDM<4>>> FCI::compute_rdm34(const int ist) 
     const int j = ij/norb_;
     const int i = ij-j*norb_;
     tmp->zero();
-    sigma_2a1(*iter, tmp);
-    sigma_2a2(*iter, tmp);
+    displacements_alpha(*iter, tmp);
+    displacements_beta(*iter, tmp);
     int kl = 0;
     for (auto t = tmp->dvec().begin(); t != tmp->dvec().end(); ++t, ++ijkl, ++kl) {
       *ebra->data(ijkl) = **t;

src/fci/knowles.h
@@ -46,9 +46,11 @@ class KnowlesHandy : public FCI {
     // run-time functions
     void sigma_1(std::shared_ptr<const Civec> cc, std::shared_ptr<Civec> sigma, std::shared_ptr<const MOFile> jop) const;
     void sigma_3(std::shared_ptr<const Civec> cc, std::shared_ptr<Civec> sigma, std::shared_ptr<const MOFile> jop) const;
-    void sigma_2b (std::shared_ptr<Dvec> d, std::shared_ptr<Dvec> e, std::shared_ptr<const MOFile> jop) const;
-    void sigma_2c1(std::shared_ptr<Civec> sigma, std::shared_ptr<const Dvec> e) const;
-    void sigma_2c2(std::shared_ptr<Civec> sigma, std::shared_ptr<const Dvec> e) const;
+    void sigma_2b (std::shared_ptr<Matrix> d, std::shared_ptr<Matrix> e, std::shared_ptr<const MOFile> jop) const;
+    void sigma_2a1(std::shared_ptr<const Civec> cc, std::shared_ptr<Matrix> d) const;
+    void sigma_2a2(std::shared_ptr<const Civec> cc, std::shared_ptr<Matrix> d) const;
+    void sigma_2c1(std::shared_ptr<Civec> sigma, std::shared_ptr<const Matrix> e) const;
+    void sigma_2c2(std::shared_ptr<Civec> sigma, std::shared_ptr<const Matrix> e) const;
 
   public:
     // this constructor is ugly... to be fixed some day...

commit 03595b2e92c9da01ae21f3fde62fbdde70acd23d

fixing the code that was broken by 6af07d51eff180a379ac73aa568b1327f0f74039

src/fci/knowles_compute.cc
@@ -90,7 +90,7 @@ shared_ptr<Dvec> KnowlesHandy::form_sigma(shared_ptr<const Dvec> ccvec, shared_p
 }
 
 void KnowlesHandy::sigma_2a1(shared_ptr<const Civec> cc, shared_ptr<Matrix> d) const {
-  assert(d->det() == cc->det());
+  assert(d->ndim() == cc->size());
   const int lb = cc->lenb();
   const int ij = nij();
   const double* const source_base = cc->data();
@@ -105,7 +105,7 @@ void KnowlesHandy::sigma_2a1(shared_ptr<const Civec> cc, shared_ptr<Matrix> d) c
 }
 
 void KnowlesHandy::sigma_2a2(shared_ptr<const Civec> cc, shared_ptr<Matrix> d) const {
-  assert(d->det() == cc->det());
+  assert(d->ndim() == cc->size());
   const int la = cc->lena();
   const int lb = cc->lenb();
   const int ij = nij();