2dbef43ff86aed34a1186ecb0fce3f42e914ba1d
Eduard Valeyev <eduard@valeyev.net>
2014-02-03 10:12:30 -0500

    Fixed incorrect use of Libint defines.

--- src/parallel/resources.h
+++ src/parallel/resources.h
@@ -61,10 +61,10 @@ namespace bagel {
     #ifdef LIBINT_INTERFACE
       // TODO 20LU should not be hardwired
       libint_t_ = std::unique_ptr<Libint_t[]>(new Libint_t[20LU*20LU*20LU*20LU]);
-      if (libint2_need_memory_3eri1(LIBINT_MAX_AM) < libint2_need_memory_eri(LIBINT_MAX_AM)) {
-        LIBINT2_PREFIXED_NAME(libint2_init_eri)(&libint_t_[0], LIBINT_MAX_AM, 0);
+      if (libint2_need_memory_3eri1(LIBINT2_MAX_AM_3ERI1) < libint2_need_memory_eri(LIBINT2_MAX_AM_ERI)) {
+        LIBINT2_PREFIXED_NAME(libint2_init_eri)(&libint_t_[0], LIBINT2_MAX_AM_ERI, 0);
       } else {
-        LIBINT2_PREFIXED_NAME(libint2_init_3eri1)(&libint_t_[0], LIBINT_MAX_AM, 0);
+        LIBINT2_PREFIXED_NAME(libint2_init_3eri1)(&libint_t_[0], LIBINT2_MAX_AM_3ERI1, 0);
       }
     #endif
     }
