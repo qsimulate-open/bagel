6badc8ffd8b35bed9eb5f1a0e5b46cb5fd3d4aae
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2014-02-11 11:49:38 -0600

    Momentum integral is built, but needs testing

--- src/integral/compos/complexmomentumbatch.cc
+++ src/integral/compos/complexmomentumbatch.cc
@@ -81,10 +81,26 @@ void ComplexMomentumBatch::perform_VRR(complex<double>* intermediate) {
   complex<double>* worksy = stack_->get<complex<double>>(worksize*worksize);
   complex<double>* worksz = stack_->get<complex<double>>(worksize*worksize);
 
+  complex<double> imag (0.0, 1.0);
+  if (swap01_) imag *= -1.0;
+  const double Bx = basisinfo_[1]->position(0);
+  const double By = basisinfo_[1]->position(1);
+  const double Bz = basisinfo_[1]->position(2);
+  const double A_Bx = basisinfo_[1]->vector_potential(0);
+  const double A_By = basisinfo_[1]->vector_potential(1);
+  const double A_Bz = basisinfo_[1]->vector_potential(2);
+  const double field_x_half = 0.5 * basisinfo_[0]->magnetic_field(0);
+  const double field_y_half = 0.5 * basisinfo_[0]->magnetic_field(1);
+  const double field_z_half = 0.5 * basisinfo_[0]->magnetic_field(2);
+  assert (basisinfo_[1]->magnetic_field(0) == field_x_half * 2.0);
+  assert (basisinfo_[1]->magnetic_field(1) == field_y_half * 2.0);
+  assert (basisinfo_[1]->magnetic_field(2) == field_z_half * 2.0);
+
   for (int ii = 0; ii != prim0_*prim1_; ++ii) {
     // Perform VRR
     int offset_ii = ii*asize_intermediate_;
     const double cop = 1.0 / xp_[ii];
+    const double cb = xb_[ii];
     const complex<double> cxpa = P_[ii*3  ]-basisinfo_[0]->position(0);
     const complex<double> cypa = P_[ii*3+1]-basisinfo_[0]->position(1);
     const complex<double> czpa = P_[ii*3+2]-basisinfo_[0]->position(2);
@@ -119,6 +135,10 @@ void ComplexMomentumBatch::perform_VRR(complex<double>* intermediate) {
       }
     }
 
+    complex<double> Sx, Sy, Sz;    // operator = 1
+    complex<double> ox, oy, oz;    // operator = x
+    complex<double> dx, dy, dz;    // operator = d/dx
+
     // now we obtain the output
     assert((ang0_+1)*(ang0_+2)*(ang1_+1)*(ang1_+2)/4 == asize_intermediate_);
 
@@ -131,9 +151,38 @@ void ComplexMomentumBatch::perform_VRR(complex<double>* intermediate) {
             for (int jy = 0; jy <= ang1_-jz; ++jy) {
               const int jx = ang1_-jy-jz;
               if (jx >= 0) {
-                current_data[cnt              ] = 0.0; //workpx[ix+amax1_*jx]*worksy[iy+amax1_*jy]*worksz[iz+amax1_*jz];
-                current_data[cnt+size_block_  ] = 0.0; //worksx[ix+amax1_*jx]*workpy[iy+amax1_*jy]*worksz[iz+amax1_*jz];
-                current_data[cnt+size_block_*2] = 0.0; //worksx[ix+amax1_*jx]*worksy[iy+amax1_*jy]*workpz[iz+amax1_*jz];
+                const double jxd = jx;
+                const double jyd = jy;
+                const double jzd = jz;
+                complex<double> minus1x = 0.0;
+                complex<double> minus1y = 0.0;
+                complex<double> minus1z = 0.0;
+                if (jx > 0) minus1x = jxd * worksx[ix+amax2*(jx-1)];
+                if (jy > 0) minus1y = jyd * worksy[iy+amax2*(jy-1)];
+                if (jz > 0) minus1z = jzd * worksz[iz+amax2*(jz-1)];
+
+                Sx = worksx[ix + amax2 * jx];
+                Sy = worksy[iy + amax2 * jy];
+                Sz = worksz[iz + amax2 * jz];
+
+                ox = worksx[ix + amax2 * (jx+1)] + Bx * worksx[ix + amax2 * jx];
+                oy = worksy[iy + amax2 * (jy+1)] + By * worksy[iy + amax2 * jy];
+                oz = worksz[iz + amax2 * (jz+1)] + Bz * worksz[iz + amax2 * jz];
+
+                dx = minus1x - 2.0 * cb * worksx[ix+amax2 * (jx+1)] - imag * A_Bx * worksx[ix + amax2 * jx];
+                dy = minus1y - 2.0 * cb * worksy[iy+amax2 * (jy+1)] - imag * A_By * worksy[iy + amax2 * jy];
+                dz = minus1z - 2.0 * cb * worksz[iz+amax2 * (jz+1)] - imag * A_Bz * worksz[iz + amax2 * jz];
+
+                current_data[cnt              ] = -imag * dx * Sy * Sz;
+                current_data[cnt+size_block_  ] = -imag * Sx * dy * Sz;
+                current_data[cnt+size_block_*2] = -imag * Sx * Sy * dz;
+
+                current_data[cnt              ] += field_y_half * Sx * Sy * oz;
+                current_data[cnt+size_block_  ] += field_z_half * ox * Sy * Sz;
+                current_data[cnt+size_block_*2] += field_x_half * Sx * oy * Sz;
+                current_data[cnt              ] -= field_z_half * Sx * oy * Sz;
+                current_data[cnt+size_block_  ] -= field_x_half * Sx * Sy * oz;
+                current_data[cnt+size_block_*2] -= field_y_half * ox * Sy * Sz;
                 ++cnt;
               }
             }
