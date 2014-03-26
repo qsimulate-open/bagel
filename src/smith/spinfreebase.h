1892a77657a8581c8ddb16f321a64e1bf5854315
Matthew MacLeod <matthew.macleod@northwestern.edu>
2014-03-26 10:24:09 -0500

    bug fix in sigma.

--- src/smith/spinfreebase.h
+++ src/smith/spinfreebase.h
@@ -144,12 +144,12 @@ class SpinFreeMethod {
       for (auto& i1 : active_) {
         for (auto& i0 : active_) {
           for (auto& ci0 : ci_) {
-            const size_t size = ci0.size();
             std::unique_ptr<double[]> fdata = f1_->get_block(i0, i1);
-            std::unique_ptr<double[]> rdata = rdm1deriv_->get_block(i0, i1);
+            std::unique_ptr<double[]> rdata = rdm1deriv_->get_block(ci0, i0, i1);
+            const size_t size = ci0.size();
             std::unique_ptr<double[]> data(new double[size]);
             dgemm_("T", "N", 1, ci0.size(), i0.size()*i1.size(),
-                    1.0, fdata, i0.size()*i1.size(), rdata, i0.size()*i1.size(), 1.0, data, 1);
+                    1.0, fdata, i0.size()*i1.size(), rdata, ci0.size()*i0.size()*i1.size(), 1.0, data, 1);
             out->put_block(data, ci0);
           }
         }
