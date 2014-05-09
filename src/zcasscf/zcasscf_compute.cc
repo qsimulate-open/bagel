c790652c36f627610a6287ed5675a7cac4bafe9e
Jefferson Bates <jefferson.bates@northwestern.edu>
2014-05-09 12:56:28 -0500

    flags introduced for convergence of the electronic and positronic subspaces.

--- src/zcasscf/zcasscf_compute.cc
+++ src/zcasscf/zcasscf_compute.cc
@@ -57,9 +57,11 @@ void ZCASSCF::compute() {
   auto ele_x = make_shared<ZMatrix>((nocc_+nvirtnr_)*2, (nocc_+nvirtnr_)*2);
   ele_x->unit();
   vector<double> ele_energy;
+  bool ele_conv = false;
   auto pos_x = make_shared<ZMatrix>(nocc_*2 + nneg_, nocc_*2 + nneg_);
   pos_x->unit();
   vector<double> pos_energy;
+  bool pos_conv = false;
 #endif
 
   shared_ptr<const ZMatrix> hcore = make_shared<RelHcore>(geom_);
@@ -309,8 +311,11 @@ void ZCASSCF::compute() {
 #ifdef BOTHSPACES
     if (gradient < thresh_) break;
 #else
-    if (gradient < thresh_) break;
+    if (gradient < thresh_ && !optimize_electrons) pos_conv = true;
+    if (gradient < thresh_ &&  optimize_electrons) ele_conv = true;
     optimize_electrons = optimize_electrons == true ? false : true;
+    if (ele_conv && optimize_electrons) optimize_electrons = false;
+    if (pos_conv && ele_conv) break;
 #endif
   }
 }
