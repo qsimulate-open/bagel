a0473588fcacd92e8f6ef7e94fe35ec7136d766d
Toru Shiozaki <shiozaki@northwestern.edu>
2014-05-26 17:40:14 -0500

    updating a utility python file. It did not work with cc-pvdz (it was designed for dyall's basis back then)

--- src/basis/source/uncontract.py
+++ src/basis/source/uncontract.py
@@ -19,31 +19,36 @@ currshell = ""
 prevprims = ""
 
 angkey = "      \"angular\" : \""
-primkey = "      \"prim\" : [" 
+primkey = "      \"prim\" : ["
+
+done = {}
 
 for i in lines:
     if len(i) == 0: out.append("")
-    elif i[0] == "{" or i[0] == "/": out.append(i) 
+    elif i[0] == "{" or i[0] == "/": out.append(i)
     elif i[0] == "}": continue
     # atom
     elif i[0:3] == "  \"":
         prevshell = ""
         prevprims = []
-       
+
         if i[3:5] != "H\"":
             out = out[:-1]
             out.append("    }")
             out.append("  ],")
+            done.clear()
         out.append(i)
         out.append("    {")
 
     elif len(i) > len(angkey) and i[0:len(angkey)] == angkey:
         currshell = i[len(angkey)]
-    elif len(i) > len(primkey) and i[0:len(primkey)] == primkey and prevshell != currshell:
+    elif len(i) > len(primkey) and i[0:len(primkey)] == primkey:
         prevshell = currshell
         if prevprims != i:
             prims = i[len(primkey):-2].split(", ")
             for j in prims:
+                if done.has_key(currshell + j): continue
+                done[currshell + j] = j
                 out.append("      \"angular\" : \"" + currshell + "\",")
                 out.append("      \"prim\" : [" + j + "],")
                 out.append("      \"cont\" : [[ 1.0 ]]")
@@ -52,8 +57,8 @@ for i in lines:
             out.append("    }, {")
         prevprims == i
 out = out[:-1]
-out.append("    }") 
-out.append("  ]") 
-out.append("}") 
+out.append("    }")
+out.append("  ]")
+out.append("}")
 
 print "\n".join(out)
