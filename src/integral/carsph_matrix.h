2f63809dc825a52e32e419a27d9cd6786603d959
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2014-06-21 15:26:00 -0500

    Tabulating the cartesian-to-spherical transformation matrices for one basis function

---
+++ src/integral/carsph_matrix.h
@@ -0,0 +1,39 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: carsph_matrix.h
+// Copyright (C) 2014 Toru Shiozaki
+//
+// Author: Ryan D. Reynolds <RyanDReynolds@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+
+#ifndef __SRC_INTEGRAL_CARSPH_MATRIX_H
+#define __SRC_INTEGRAL_CARSPH_MATRIX_H
+
+#include <memory>
+#include <src/math/matrix.h>
+
+namespace bagel {
+
+std::shared_ptr<Matrix> carsph_matrix (const int i);
+
+}
+
+#endif
