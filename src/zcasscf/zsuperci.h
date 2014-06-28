3ced642be4e7c4ec63b2f2c79a5f3681f8bf76df
Jefferson Bates <jbates27.2@gmail.com>
2014-06-28 12:06:38 -0500

    modified default option : diis to start on iteration 5 as in the non-rel case.

--- src/zcasscf/zsuperci.h
+++ src/zcasscf/zsuperci.h
@@ -38,7 +38,7 @@ class ZSuperCI : public ZCASSCF {
 
     void common_init() {
       std::cout << "   * Using the Super CI algorithm as noted in Roos (1980) IJQC *    " << std::endl;
-      diis_start_ = idata_->get<int>("diis_start", 100);
+      diis_start_ = idata_->get<int>("diis_start", 5);
       std::cout << "   * DIIS will be used after " << diis_start_ << " macro iterations *    " << std::endl << std::endl;
     }
 
