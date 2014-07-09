qsimulate-open/bagel

commit d8285e082dd5ae3818be6b3f80d17fb2902cc5d3

per BTAS discussions. transform in DF and slice in Matrix have different signature now. Fock constructor accepts both coefficient and its shared_ptr.

src/math/matview.h
@@ -23,15 +23,20 @@
 // the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 
+#ifdef MATRIX_BASE
 #ifndef __SRC_MATH_MATVIEW_H
 #define __SRC_MATH_MATVIEW_H
 
 #include <complex>
 #include <src/math/btas_interface.h>
+#include <src/math/matrix_base.h>
 
 namespace bagel {
 
-template<typename DataType>
+template <typename DataType>
+class Matrix_base;
+
+template <typename DataType>
 class MatView_ : public btas::TensorView2<DataType> {
   protected:
     bool localized_;
@@ -84,10 +89,13 @@ class MatView_ : public btas::TensorView2<DataType> {
     }
 
   public:
+    MatView_() { }
     MatView_(const MatView_& o) : btas::TensorView2<DataType>(o), localized_(o.localized()) { init(); }
     MatView_(const btas::TensorView2<DataType>& o, const bool lo) : btas::TensorView2<DataType>(o), localized_(lo) { init(); }
     MatView_(btas::TensorView2<DataType>&& o, const bool lo) : btas::TensorView2<DataType>(std::move(o)), localized_(lo) { init(); }
     MatView_(const btas::CRange<2>& r, const typename btas::Tensor2<DataType>::storage_type& s, const bool lo) : btas::TensorView2<DataType>(r, s), localized_(lo) { init(); }
+    MatView_(Matrix_base<DataType>& o) : btas::TensorView2<DataType>(o), localized_(o.localized()) { init(); }
+    MatView_(const Matrix_base<DataType>& o) : btas::TensorView2<DataType>(o), localized_(o.localized()) { init(); }
 
     int ndim() const { return this->extent(0); }
     int mdim() const { return this->extent(1); }
@@ -152,4 +160,5 @@ using ZMatView = MatView_<std::complex<double>>;
 
 }
 
+#endif
 #endif

btas/tensor.h
@@ -28,7 +28,7 @@ namespace btas {
            class _Storage = btas::DEFAULT::storage<_T>,
            class = typename std::enable_if<std::is_same<_T, typename _Storage::value_type>::value>::type
           >
-  class Tensor : public TensorView<_T,_Range,_Storage> {
+  class Tensor {
 
     public:
 
@@ -79,24 +79,20 @@ namespace btas {
       template<typename... _args>
       explicit
       Tensor (const size_type& first, const _args&... rest) :
-      TensorView<_T,_Range,_Storage>(range_type(first, rest...))
+      range_(range_type(first, rest...))
       {
         // TODO make this disablable in all constructors
         //assert(range_.ordinal(range_.lobound()) == 0);
         array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// construct from \c range, allocate data, but not initialized
       template <typename Range>
       explicit
       Tensor (const Range& range, typename std::enable_if<btas::is_boxrange<Range>::value>::type* = 0) :
-      TensorView<_T,_Range,_Storage>(range_type(range.lobound(), range.upbound()))
+      range_(range.lobound(), range.upbound())
       {
         array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// construct from \c range object, set all elements to \c v
@@ -105,12 +101,10 @@ namespace btas {
       Tensor (const Range& range,
               value_type v,
               typename std::enable_if<btas::is_boxrange<Range>::value>::type* = 0) :
-              TensorView<_T,_Range,_Storage>(range_type(range.lobound(), range.upbound()))
+              range_(range.lobound(), range.upbound())
       {
         array_adaptor<storage_type>::resize(storage_, range_.area());
         std::fill(begin(), end(), v);
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// construct from \c range and \c storage
@@ -122,48 +116,40 @@ namespace btas {
                                       not std::is_same<Range,range_type>::value &
                                       not std::is_same<Storage,storage_type>::value
                                      >::type* = 0) :
-      TensorView<_T,_Range,_Storage>(range_type(range.lobound(), range.upbound())), storage_(storage)
+      range_(range.lobound(), range.upbound()), storage_(storage)
       {
         if (storage_.size() != range_.area())
           array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// copy-copy-construct from \c range and \c storage
       explicit
       Tensor (const range_type& range, const storage_type& storage) :
-      TensorView<_T,_Range,_Storage>(range_type(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound()))),
+      range_(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound())),
       storage_(storage)
       {
         if (storage_.size() != range_.area())
           array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// copy-move-construct from \c range and \c storage
       explicit
       Tensor (const range_type& range, storage_type&& storage) :
-      TensorView<_T,_Range,_Storage>(range_type(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound()))),
+      range_(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound())),
       storage_(storage)
       {
         if (storage_.size() != range_.area())
           array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// move-construct from \c range and \c storage
       explicit
       Tensor (range_type&& range, storage_type&& storage) :
-      TensorView<_T,_Range,_Storage>(range_type(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound()))),
+      range_(range.ordinal(*range.begin()) == 0 ? range : range_type(range.lobound(), range.upbound())),
       storage_(storage)
       {
         if (storage_.size() != range_.area())
           array_adaptor<storage_type>::resize(storage_, range_.area());
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// Construct an evaluated tensor
@@ -181,13 +167,11 @@ namespace btas {
       explicit
       Tensor (const Range& range, InIter it, const Op& op,
               typename std::enable_if<btas::is_boxrange<Range>::value>::type* = 0) :
-              TensorView<_T,_Range,_Storage>(range_type(range.lobound(), range.upbound()))
+              range_(range.lobound(), range.upbound())
       {
         auto size = range_.area();
         array_adaptor<storage_type>::resize(storage_, size);
         std::transform(it, it+size, begin(), op);
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// copy constructor
@@ -195,28 +179,22 @@ namespace btas {
       template<class _Tensor, class = typename std::enable_if<is_boxtensor<_Tensor>::value>::type>
       Tensor (const _Tensor& x)
         :
-        TensorView<_T,_Range,_Storage>(range_type(x.range().lobound(), x.range().upbound())),
+        range_ (x.range().lobound(), x.range().upbound()),
         storage_(x.cbegin(),x.cend())
       {
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// copy constructor
       explicit
       Tensor (const Tensor& x)
-      : TensorView<_T,_Range,_Storage>(range_type(x.range())), storage_(x.storage_)
+      : range_ (x.range()), storage_(x.storage_)
       {
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// move constructor
       Tensor (Tensor&& x)
-      : TensorView<_T,_Range,_Storage>(range_type(x.range())), storage_(x.storage_)
+      : range_ (x.range()), storage_(x.storage_)
       {
-        // setting storage_ref to the base
-        this->set_storage(storage_);
       }
 
       /// copy assignment operator
@@ -368,7 +346,12 @@ namespace btas {
         return std::numeric_limits<size_type>::max();
       }
 
-      using TensorView<_T,_Range,_Storage>::empty;
+      /// test whether Tensor is empty
+      bool
+      empty() const
+      {
+        return range_.area() == 0;
+      }
 
       /// swap this and x
       void
@@ -631,7 +614,7 @@ namespace btas {
 
     private:
 
-      using TensorView<_T,_Range,_Storage>::range_;///< range object
+      range_type range_;///< range object
       storage_type storage_;///< data
 
   }; // end of Tensor

btas/tensorview.h
@@ -75,13 +75,6 @@ namespace btas {
       {
       }
 
-      /// construct from \c range. \c storage will be set later
-      explicit
-      TensorView (const range_type& range)
-      : range_(range)
-      {
-      }
-
       /// construct from \c range and \c storage
       template <typename S = _Storage>
       explicit
@@ -94,13 +87,6 @@ namespace btas {
       explicit
       TensorView (range_type&& range, storage_type&& storage) :
       range_(range), storageref_(storage)
-      {
-      }
-
-      /// construct from \c range. \c storage will be set later
-      explicit
-      TensorView (range_type&& range)
-      : range_(range)
       {
       }
 
@@ -414,16 +400,10 @@ namespace btas {
       }
 #endif
 
-    protected:
+    private:
 
       range_type range_;///< range object
       storageref_type storageref_;///< dataref
-
-      /// set storage object
-      template <typename _S = _Storage>
-      void set_storage(_S& storage) {
-        storageref_ = storage;
-      }
 
   }; // end of TensorView
 

src/casscf/casbfgs.cc
@@ -78,7 +78,7 @@ void CASBFGS::compute() {
 
     // compute one-body operators
     // * preparation
-    shared_ptr<const MatView> ccoeff = coeff_->slice(0, nclosed_);
+    const MatView ccoeff = coeff_->slice(0, nclosed_);
     // * core Fock operator
     shared_ptr<const Matrix> cfockao = nclosed_ ? make_shared<const Fock<1>>(geom_, hcore_, nullptr, ccoeff, /*store*/false, /*rhf*/true) : hcore_;
     shared_ptr<const Matrix> cfock = make_shared<Matrix>(*coeff_ % *cfockao * *coeff_);

src/casscf/casscf.cc
@@ -189,7 +189,7 @@ void CASSCF::one_body_operators(shared_ptr<Matrix>& f, shared_ptr<Matrix>& fact,
     auto acoeff = coeff_->slice(nclosed_, nclosed_+nact_);
 
     finact = make_shared<Matrix>(*coeff_ % *fci_->jop()->core_fock() * *coeff_);
-    auto fact_ao = make_shared<Fock<1>>(geom_, hcore_->clone(), nullptr, make_shared<Matrix>(*acoeff * *rdm1mat), false, /*rhf*/true);
+    auto fact_ao = make_shared<Fock<1>>(geom_, hcore_->clone(), nullptr, acoeff * *rdm1mat, false, /*rhf*/true);
     f = make_shared<Matrix>(*finact + *coeff_% *fact_ao * *coeff_);
   }
   {
@@ -284,20 +284,20 @@ shared_ptr<const Coeff> CASSCF::semi_canonical_orb() const {
   copy_n(fci_->rdm1_av()->data(), rdm1mat->size(), rdm1mat->data());
   rdm1mat->sqrt();
   rdm1mat->scale(1.0/sqrt(2.0));
-  auto ocoeff = nclosed_ ? coeff_->slice(0, nclosed_) : nullptr;
+  auto ocoeff = nclosed_ ? coeff_->slice(0, nclosed_) : MatView();
   auto acoeff = coeff_->slice(nclosed_, nocc_);
   auto vcoeff = coeff_->slice(nocc_, nbasis_);
 
   unique_ptr<double[]> eig(new double[coeff_->mdim()]);
-  Fock<1> fock(geom_, fci_->jop()->core_fock(), nullptr, make_shared<Matrix>(*acoeff * *rdm1mat), false, /*rhf*/true);
+  Fock<1> fock(geom_, fci_->jop()->core_fock(), nullptr, acoeff * *rdm1mat, false, /*rhf*/true);
   Matrix trans(nbasis_, nbasis_);
   trans.unit();
   if (nclosed_) {
-    Matrix ofock = *ocoeff % fock * *ocoeff;
+    Matrix ofock = ocoeff % fock * ocoeff;
     ofock.diagonalize(eig.get());
     trans.copy_block(0, 0, nclosed_, nclosed_, ofock);
   }
-  Matrix vfock = *vcoeff % fock * *vcoeff;
+  Matrix vfock = vcoeff % fock * vcoeff;
   vfock.diagonalize(eig.get());
   trans.copy_block(nocc_, nocc_, nvirt_, nvirt_, vfock);
   return make_shared<Coeff>(*coeff_ * trans);

src/casscf/jkop.h
@@ -48,7 +48,7 @@ class JKop {
          const std::shared_ptr<FCI> fci, const size_t nocc, const size_t nclosed, const size_t nact)
     : fci_(fci), coeff_(c), nocc_(nocc), nclosed_(nclosed), nbasis_(df->nbasis0()) {
 
-      std::shared_ptr<const MatView> ocoeff = coeff_->slice(0, nocc);
+      const MatView ocoeff = coeff_->slice(0, nocc);
       assert(nclosed+nact == nocc);
       assert(df->nbasis0() == df->nbasis1());
       // K operator // (ai|ai)

src/casscf/jvec.cc
@@ -33,7 +33,7 @@ Jvec::Jvec(shared_ptr<FCI> fci, shared_ptr<const Coeff> coeff, const size_t nclo
 
   shared_ptr<const DFDist> df = fci->geom()->df();
   const size_t nocc = nclosed+nact;
-  shared_ptr<const MatView> cc = coeff->slice(0, nocc);
+  const MatView cc = coeff->slice(0, nocc);
 
   shared_ptr<RDM<1>> rdm1_av = fci->rdm1_av();
   shared_ptr<RDM<2>> rdm = fci->rdm2_av();

src/casscf/supercigrad.cc
@@ -51,7 +51,7 @@ std::shared_ptr<GradFile> GradEval<SuperCIGrad>::compute() {
   const int nact = ref_->nact();
   const int nocc = ref_->nocc();
 
-  shared_ptr<const MatView> ocoeff = ref_->coeff()->slice(0,nocc);
+  const MatView ocoeff = ref_->coeff()->slice(0,nocc);
 
   // state-averaged density matrices
   shared_ptr<const RDM<1>> rdm1_av = task_->fci()->rdm1_av();
@@ -70,7 +70,7 @@ std::shared_ptr<GradFile> GradEval<SuperCIGrad>::compute() {
   // 1/2 Y_ri = hd_ri + K^{kl}_{rj} D^{lk}_{ji}
   //          = hd_ri + (kr|G)(G|jl) D(lj, ki)
   // 1) one-electron contribution
-  auto hmo = make_shared<const Matrix>(*ref_->coeff() % *ref_->hcore() * *ocoeff);
+  auto hmo = make_shared<const Matrix>(*ref_->coeff() % *ref_->hcore() * ocoeff);
   shared_ptr<const Matrix> rdm1 = ref_->rdm1_mat(target);
   assert(rdm1->ndim() == nocc && rdm1->mdim() == nocc);
   g0->add_block(2.0, 0, 0, nmobasis, nocc, *hmo * *rdm1);
@@ -134,18 +134,18 @@ std::shared_ptr<GradFile> GradEval<SuperCIGrad>::compute() {
   shared_ptr<const DFFullDist> qij  = halfjj->compute_second_transform(ocoeff);
   shared_ptr<DFHalfDist> qri;
   {
-    shared_ptr<const Matrix> ztrans = make_shared<Matrix>(*ref_->coeff() * *zmat->slice(0,nocc));
+    shared_ptr<const Matrix> ztrans = make_shared<Matrix>(*ref_->coeff() * zmat->slice(0,nocc));
     {
       const RDM<2> D(*ref_->rdm2(target)+*zrdm2);
       const RDM<1> dd(*ref_->rdm1(target)+*zrdm1);
 
       shared_ptr<DFFullDist> qijd = qij->apply_2rdm(D, dd, nclosed, nact);
-      qijd->ax_plus_y(2.0, halfjj->compute_second_transform(ztrans)->apply_2rdm(*rdm2_av, *rdm1_av, nclosed, nact));
+      qijd->ax_plus_y(2.0, halfjj->compute_second_transform(*ztrans)->apply_2rdm(*rdm2_av, *rdm1_av, nclosed, nact));
       qri = qijd->back_transform(ocoeff);
     }
     {
       shared_ptr<const DFFullDist> qijd2 = qij->apply_2rdm(*rdm2_av, *rdm1_av, nclosed, nact);
-      qri->ax_plus_y(2.0, qijd2->back_transform(ztrans));
+      qri->ax_plus_y(2.0, qijd2->back_transform(*ztrans));
     }
   }
 

commit 79409130cd7f4b12f1e52f115adb13bdc2a23fea

minor. Still have a bug caused by d8285e082dd5ae3818be6b3f80d17fb2902cc5d3 in SA-CASSCF gradients

src/math/matview.h
@@ -97,6 +97,9 @@ class MatView_ : public btas::TensorView2<DataType> {
     MatView_(Matrix_base<DataType>& o) : btas::TensorView2<DataType>(o), localized_(o.localized()) { init(); }
     MatView_(const Matrix_base<DataType>& o) : btas::TensorView2<DataType>(o), localized_(o.localized()) { init(); }
 
+    MatView_<DataType> operator=(const MatView_<DataType>& o) { localized_ = o.localized_; btas::TensorView2<DataType>::operator=(o); return *this; }
+    MatView_<DataType> operator=(MatView_<DataType>&& o)      { localized_ = o.localized_; btas::TensorView2<DataType>::operator=(o); return *this; }
+
     int ndim() const { return this->extent(0); }
     int mdim() const { return this->extent(1); }
     size_t size() const { return ndim() * mdim(); }