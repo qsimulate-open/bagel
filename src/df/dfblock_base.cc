30daffc9984273ed5f8d8f5e551b3762c0202149
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2014-07-09 11:49:11 -0500

    Further merge conflict resolution
    Links and passes all test cases now.  Some cleanup is still needed (particularly concerning ComplexDF vs. DF_London).

---
+++ src/df/dfblock_base.cc
@@ -0,0 +1,30 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: dfblock_base.cc
+// Copyright (C) 2014 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <src/df/dfblock_base.h>
+
+using namespace std;
+template class bagel::DFBlock_base<double>;
+template class bagel::DFBlock_base<complex<double>>;
