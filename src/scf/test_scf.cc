7d43e71354d21931fcc8d29cd9f1a5eeef3c08b1
Hai-Anh Le <anh@u.northwestern.edu>
2014-09-02 19:15:07 -0500

    minor

--- src/scf/test_scf.cc
+++ src/scf/test_scf.cc
@@ -95,7 +95,7 @@ BOOST_AUTO_TEST_CASE(DF_HF) {
     BOOST_CHECK(compare(scf_energy("hc_svp_rohf"),        -38.16810629));
     BOOST_CHECK(compare(scf_energy("hf_new_dfhf"),        -99.97989929));
     BOOST_CHECK(compare(scf_energy("hcl_svp_dfhf"),      -459.93784632));
-    BOOST_CHECK(compare(scf_energy("ecp_cuh2"),          -196.12254012));
+    BOOST_CHECK(compare(scf_energy("hf_ecp_cuh2"),       -196.12254012));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
