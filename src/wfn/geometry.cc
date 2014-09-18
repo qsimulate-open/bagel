fe27f79f9c89e95f52f37349f104ab15cd5a382e
Hai-Anh Le <anh@u.northwestern.edu>
2014-09-18 15:55:36 -0500

    fix lattice vectors

--- src/wfn/geometry.cc
+++ src/wfn/geometry.cc
@@ -70,8 +70,6 @@ Geometry::Geometry(shared_ptr<const PTree> geominfo) : magnetism_(false) {
     for (auto& ivec : *vectors) {
       string id = "a" + to_string(dim+1);
       array<double, 3> vec = ivec->get_array<double, 3>(id);
-      const double norm = sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
-      for (int i = 1; i != 3; ++i) vec[i] /= norm;
       primitive_vectors_.push_back(vec);
       ++dim;
     }
