10fed6dcb37f7ed6a38e9e3e5754549dc3bcab67
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2014-09-28 00:47:14 -0500

    On construction, check that a QuatMatrix actually has the right symmetry.

--- src/math/quatmatrix.h
+++ src/math/quatmatrix.h
@@ -32,10 +32,28 @@
 namespace bagel {
 
 class QuatMatrix : public ZMatrix {
+  protected:
+    bool quat_symmetry(const double thresh = 1.0e-8) {
+      assert(mdim()%2 == 0 && ndim()%2 == 0);
+      const int m = mdim()/2;
+      const int n = ndim()/2;
+
+      std::shared_ptr<ZMatrix> u = get_submatrix(0, 0, n, m);
+      std::shared_ptr<ZMatrix> v = get_submatrix(n, 0, n, m);
+      u->ax_plus_y(-1.0, *get_submatrix(n, m, n, m)->get_conjg());
+      v->ax_plus_y( 1.0, *get_submatrix(0, m, n, m)->get_conjg());
+
+      const double val = u->norm() + v->norm();
+      const bool out = val/(n*m) < thresh;
+      if (!out)
+        std::cout << std::fixed << std::setprecision(12) << "Time-reversal symmetry not satisfied; error norm = " << val/(n*m) << std::endl;
+      return out;
+    }
+
   public:
-    QuatMatrix(const ZMatrix& o) : ZMatrix(o) { }
-    QuatMatrix(ZMatrix&& o) : ZMatrix(std::move(o)) { }
-    // TODO : implement symmetry checking function, and constructor that can take "00" and "01" matrices and build the rest of the matrix
+    QuatMatrix(const ZMatrix& o) : ZMatrix(o) { assert(quat_symmetry()); }
+    QuatMatrix(ZMatrix&& o) : ZMatrix(std::move(o)) { assert(quat_symmetry()); }
+    // TODO : implement constructor that can take "00" and "01" matrices and build the rest of the matrix
 
     void diagonalize(VecView eig) override {
       assert(ndim() == mdim());
