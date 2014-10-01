47032aee263735e0b4e1152fdea9471dd4fe26b8
Shane Parker <shane.parker@u.northwestern.edu>
2014-10-01 16:20:20 -0500

    implements a specialized kronecker product. I imagine it could benefit from threading but all of my attempts to improve it have made it worse.

--- src/asd_dmrg/kronecker.cc
+++ src/asd_dmrg/kronecker.cc
@@ -1,6 +1,6 @@
 //
 // BAGEL - Parallel electron correlation program.
-// Filename: kronecker.h
+// Filename: kronecker.cc
 // Copyright (C) 2014 Toru Shiozaki
 //
 // Author: Toru Shiozaki <shiozaki@northwestern.edu>
@@ -23,41 +23,32 @@
 // the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 
-#ifndef __SRC_ASD_DMRG_KRONECKER_H
-#define __SRC_ASD_DMRG_KRONECKER_H
+#include <src/asd_dmrg/kronecker.h>
+#include <src/math/matrix.h>
+#include <src/util/taskqueue.h>
 
-#include <src/math/algo.h>
-#include <src/parallel/mpi_interface.h>
-#include <src/math/matop.h>
+using namespace bagel;
+using namespace std;
 
-namespace bagel {
-
-template <class T,
-          class = typename std::enable_if<detail::is_mat<T>::value>::type
-         >
-T kronecker_product(const bool atrans, const T& A, const bool btrans, const T& B) {
+template <>
+Matrix bagel::kronecker_product(const bool atrans, const Matrix& A, const bool btrans, const Matrix& B) {
   const int n = atrans ? A.mdim() : A.ndim();
   const int m = atrans ? A.ndim() : A.mdim();
   const int p = btrans ? B.mdim() : B.ndim();
   const int q = btrans ? B.ndim() : B.mdim();
 
-  std::shared_ptr<const T> BB = btrans ? B.transpose() : nullptr;
-  const T* Bptr = btrans ? BB.get() : &B;
+  std::shared_ptr<const Matrix> BB = btrans ? B.transpose() : nullptr;
+  const Matrix* Bptr = btrans ? BB.get() : &B;
 
-  T out(n*p, m*q);
-  for (int ja = 0; ja < m; ++ja) {
-    for (int ia = 0; ia < n; ++ia) {
-      const double aval = atrans ? A(ja, ia) : A(ia, ja);
-      out.add_block(aval, ia*p, ja*q, p, q, *Bptr);
+  Matrix out(n*p, m*q);
+  const int astride = atrans ? A.ndim() : 1;
+
+  for (int jb = 0; jb < q; ++jb) {
+    for (int ja = 0; ja < m; ++ja) {
+      const double* Adata = atrans ? A.element_ptr(ja, 0) : A.element_ptr(0, ja);
+      dger_(p, n, 1.0, Bptr->element_ptr(0, jb), 1, Adata, astride, out.element_ptr(0, jb + ja*q), p);
     }
   }
 
   return out;
 }
-
-template <>
-Matrix kronecker_product(const bool atrans, const Matrix& A, const bool btrans, const Matrix& B);
-
-}
-
-#endif
