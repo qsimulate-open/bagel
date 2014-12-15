82708c383afbdbd78fb7fa57c951a74114d057af
Jefferson Bates <jbates27.2@gmail.com>
2014-12-15 11:24:37 -0600

    minor bug fix in FCI. obsolete comment removed.

--- src/zcasscf/zqvec.cc
+++ src/zcasscf/zqvec.cc
@@ -31,7 +31,6 @@ using namespace std;
 using namespace bagel;
 
 
-// constructor to return qvec only in the electronic space (no positronic contributions)
 ZQvec::ZQvec(const int nbasis, const int nact, shared_ptr<const Geometry> geom, shared_ptr<const ZMatrix> rcoeff, shared_ptr<const ZMatrix> acoeff, const int nclosed,
              shared_ptr<const ZHarrison> fci, const bool gaunt, const bool breit)
  : ZMatrix(nbasis, nact*2) {
