cdf403f559869554489b4f5d7a0064165b051754
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2014-12-16 17:17:23 -0600

    Necessary to compile on Zinc
    In coeff.cc, slice() is called with mend = mstart+1.  The compiler thus strips out the assert(mend >= mstart) check in optimization.  In the rare case where mstart+1 causes integer overflow, this would lead to different behavior in optimized and non-optimized machine code, so it triggers the -Wstrict-overflow flag.
    
    In principle we should be able to run this check, but I don't know of any cases where t's actually been triggered, so for now I will remove it.  Not sure why I didn't get the same error when compiling locally.

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -69,18 +69,16 @@ class Matrix : public Matrix_base<double>, public std::enable_shared_from_this<M
     std::shared_ptr<Matrix> merge(const std::shared_ptr<const Matrix> o) const { return this->merge_impl<Matrix>(o); }
 
     MatView slice(const int mstart, const int mend) {
-      assert(mstart >= 0 && mend <= mdim() && mend >= mstart);
+      assert(mstart >= 0 && mend <= mdim());
       auto low = {0, mstart};
       auto up  = {ndim(), mend};
-      assert(mstart >= 0 && mend <= mdim());
       return MatView(btas::make_rwview(this->range().slice(low, up), this->storage()), localized_);
     }
 
     const MatView slice(const int mstart, const int mend) const {
-      assert(mstart >= 0 && mend <= mdim() && mend >= mstart);
+      assert(mstart >= 0 && mend <= mdim());
       auto low = {0, mstart};
       auto up  = {ndim(), mend};
-      assert(mstart >= 0 && mend <= mdim());
       return MatView(btas::make_rwview(this->range().slice(low, up), this->storage()), localized_);
     }
 
