d1a8d03b3c42882d48555d27f51979ff77b35e88
Toru Shiozaki <shiozaki@northwestern.edu>
2015-01-15 10:47:13 -0600

    minor

--- src/util/math/quatmatrix.h
+++ src/util/math/quatmatrix.h
@@ -34,7 +34,7 @@ namespace bagel {
 class QuatMatrix : public ZMatrix {
   protected:
     // Check that the matrix is symmetric under time-reversal
-    bool is_t_symmetric(const double thresh = 1.0e-8) {
+    bool is_t_symmetric(const double thresh = 1.0e-8) const {
       assert(mdim()%2 == 0 && ndim()%2 == 0);
       const int m = mdim()/2;
       const int n = ndim()/2;
