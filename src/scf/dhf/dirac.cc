453cded014c1ec2ba16575e9701d3053abb62650
Toru Shiozaki <shiozaki@northwestern.edu>
2015-04-30 14:40:30 -0500

    bug fix for the coulomb opt test - another reason why we need better coeff classes

--- src/scf/dhf/dirac.cc
+++ src/scf/dhf/dirac.cc
@@ -184,7 +184,11 @@ void Dirac::print_eig() const {
 shared_ptr<const Reference> Dirac::conv_to_ref() const {
   // we store only positive state coefficients
   const size_t npos = coeff_->mdim() - nneg_;
-  auto out = make_shared<RelReference>(geom_, coeff_, energy_, nneg_, nele_, 0, npos-nele_, gaunt_, breit_);
+  // coeff is occ, virt, nneg
+  shared_ptr<ZMatrix> c = coeff_->clone();
+  c->copy_block(0, 0, c->ndim(), npos, coeff_->slice(nneg_, npos));
+  c->copy_block(0, npos, c->ndim(), nneg_, coeff_->slice(0, nneg_));
+  auto out = make_shared<RelReference>(geom_, c, energy_, nneg_, nele_, 0, npos-nele_, gaunt_, breit_);
   vector<double> eigp(eig_.begin()+nneg_, eig_.end());
   vector<double> eigm(eig_.begin(), eig_.begin()+nneg_);
   VectorB eig(eig_.size());
