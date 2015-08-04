d4155b38c82379638125b42bb4e661e79e43186a
Hai-Anh Le <anh@u.northwestern.edu>
2015-08-04 10:45:03 -0500

    minor (seems like there is no numerical problem, boost and gsl agree with bagel at least to 1e-14)

--- src/periodic/testcode/main.cc
+++ src/periodic/testcode/main.cc
@@ -36,20 +36,24 @@ for (int i = 0; i != 100; ++i) {
 void test_gamma_lower_scaled() {
 
 // Test scaled lower gamma function for large argument
-double x = 15.0;
+double x = 100.0;
 const double beta = sqrt(pi__);
 bagel::Gamma_lower_scaled gamma_s;
 for (int i = 0; i != 10; ++i) {
   for (int j = 0; j != 10; ++j) {
     const double l = j + 0.5;
     const double z = pi__ * pow(x, 2.0);
-    cout << "(l, x) = (" << l << ", " << setprecision(5) << z << ")" << endl;
-    cout << "boost      = " << setw(20) << setprecision(12) << boost::math::tgamma_lower(l, z)/pow(x, j+1.0) << endl;
-    cout << "bagel      = " << setw(20) << setprecision(12) << gamma_s(j, z, beta) << endl;
-    cout << "gsl        = " << setw(20) << setprecision(12) << (gsl_sf_gamma(l) - gsl_sf_gamma_inc(l, z))/pow(x, j+1.0) << endl;
-    cout << endl;
+    const double boost_gamma = boost::math::tgamma_lower(l, z)/pow(x, j+1.0);
+    const double bagel_gamma = gamma_s(j, z, beta);
+    const double gsl_gamma = (gsl_sf_gamma(l) - gsl_sf_gamma_inc(l, z))/pow(x, j+1.0);
+    if (abs(bagel_gamma - boost_gamma) > 1e-14 || abs(bagel_gamma - gsl_gamma) > 1e-14 || abs(boost_gamma - gsl_gamma) > 1e-14) {
+      cout << "(l, z) = (" << l << ", " << setprecision(5) << z << ")" << endl;
+      cout << "boost      = " << setw(20) << setprecision(12) << boost_gamma << endl;
+      cout << "bagel      = " << setw(20) << setprecision(12) << bagel_gamma << endl;
+      cout << "gsl        = " << setw(20) << setprecision(12) << gsl_gamma << endl << endl;
+    }
   }
-  x += 1.0;
+  x += 0.1;
 }
 
 };
@@ -59,8 +63,8 @@ void test_mlm() {
 
   vector<complex<double>> mlm;
   const int ws = 1;
-  const int lmax = 4;
-  const int limit = 20;
+  const int lmax = 10;
+  const int limit = 10;
   const int thresh = PRIM_SCREEN_THRESH;
   mlm = compute_mlm(ws, lmax, limit, thresh);
   int cnt = 0;
