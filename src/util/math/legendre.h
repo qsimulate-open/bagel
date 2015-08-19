0ce7c416b60b714a8b40f2cdeb7532a2d0985fec
Hai-Anh Le <anh@u.northwestern.edu>
2015-08-19 17:05:48 -0500

    include renormalised associated legendre polynomial

--- src/util/math/legendre.h
+++ src/util/math/legendre.h
@@ -28,17 +28,17 @@
 #ifndef __BAGEL_MATH_LEGENDRE_H
 #define __BAGEL_MATH_LEGENDRE_H
 
-#include <src/util/math/factorial.h>
+#include <stdexcept>
+#include <src/util/constants.h>
 
 namespace bagel {
 
 class Legendre {
 
   private:
-    const Factorial f;
 
   public:
-    Legendre() : f() {}
+    Legendre() {}
     ~Legendre() {}
 
     double compute(const int l, const int am, const double x) const {
@@ -68,6 +68,51 @@ class Legendre {
             pmmp1 = plm;
           }
           return plm;
+        }
+      }
+    }
+};
+
+
+class Legendre_renorm {
+
+  public:
+    Legendre_renorm() { }
+    ~Legendre_renorm() { }
+
+
+    double compute(const int l, const int am, const double x) const {
+      if (am < 0 || am > l || fabs(x) > 1.0)
+        throw std::runtime_error("Legendre: m must be in [0, l] and x in [-1, 1]");
+      double pmm = 1.0;
+      if (am > 0) {
+        double omx2 = (1.0 - x) * (1.0 + x);
+        double fact = 1.0;
+        for (int i = 1; i <= am; ++i) {
+          pmm *= omx2 * fact / (fact + 1.0);
+          fact += 2.0;
+        }
+      }
+      pmm = sqrt((2 * am + 1) * pmm / (4.0 * pi__));
+      if (am & 1)
+        pmm = -pmm;
+      if (l == am) {
+        return pmm;
+      } else {
+        double pmmp1 = x * sqrt(2.0 * am + 3.0) * pmm;
+        if (l == am + 1) {
+          return pmmp1;
+        } else {
+          double oldfact = sqrt(2.0 * am + 3.0);
+          double pll;
+          for (int ll = am + 2; ll <= l; ++ll) {
+            double fact = sqrt((4.0 * ll * ll - 1.0) / (ll * ll - am * am));
+            pll = (x * pmmp1 - pmm / oldfact) * fact;
+            oldfact = fact;
+            pmm = pmmp1;
+            pmmp1 = pll;
+          }
+          return pll;
         }
       }
     }
