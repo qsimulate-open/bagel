0bd9906f39d300fd0a7f86d2e9b67676e28a5842
Hai-Anh Le <anh@u.northwestern.edu>
2015-08-21 14:50:40 -0500

    print nonnegative m only

--- src/periodic/testcode/interpolate/main.cc
+++ src/periodic/testcode/interpolate/main.cc
@@ -20,16 +20,18 @@ int main() {
 
   vector<complex<double>> mlm;
   const int ws = 1;
-  const int lmax = 5;
-  const int limit = 2;
+  const int lmax = 8;
+  const int limit = 20;
+
+
   LocalExps test(ws, lmax, limit);
-  int cnt = 0;
   for (int l = 0; l <= lmax; ++l) {
-    for (int mm = 0; mm <= 2*l+1; ++mm, ++cnt) {
-      const int m = mm - l;
-      const double mlm = test.mlm_real(cnt);
-      if (abs(mlm) > 1e-10)
-        cout << "l = " << l << "  m = " << m << "  mlm = " << setw(20) << setprecision(14) << mlm << endl;
+    for (int m = 0; m <= l; ++m) { // Mlm = -Ml-m
+      const int imul = l * l + m + l;
+      const double mlm = test.mlm_real(imul);
+      const double tmp = test.mlm_imag(imul);
+      if (abs(mlm) > 1e-8)
+        cout << "l = " << l << "  m = " << m << "  mlm = " << setw(20) << setprecision(14) << mlm << "     " << tmp << endl;
     }
   }
 
