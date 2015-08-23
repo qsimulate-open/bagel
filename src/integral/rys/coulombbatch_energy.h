0b340908000ad0f53eab36e132c942425f5ea5b3
Toru Shiozaki <shiozaki@northwestern.edu>
2015-08-22 23:53:56 -0500

    preparing for spin-dipole integral.

--- src/integral/rys/coulombbatch_energy.h
+++ src/integral/rys/coulombbatch_energy.h
@@ -43,7 +43,7 @@ class CoulombBatch_energy : public CoulombBatch_Base<double> {
   public:
 
     CoulombBatch_energy(const std::array<std::shared_ptr<const Shell>,2>& _info, const std::shared_ptr<const Molecule> mol, std::shared_ptr<StackMem> stack = nullptr)
-      :  CoulombBatch_Base<double>(_info, mol, 0, stack) {};
+      :  CoulombBatch_Base<double>(_info, mol, 0, 0, stack) {};
 
      ~CoulombBatch_energy() {};
 
