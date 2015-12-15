71fa3cfbb9b08d724cc960774ae012f265b4dbba
Toru Shiozaki <shiozaki@northwestern.edu>
2015-12-15 12:12:16 -0600

    pretty up the output

--- src/util/timer.h
+++ src/util/timer.h
@@ -61,8 +61,7 @@ class Timer {
         title = to_upper(title);
         std::cout << "    * " << std::left << std::setw(39) << title << std::right << std::setw(10) << std::setprecision(2) << tick() << std::endl;
 #ifdef HAVE_MPI_H
-//    } else if (level_ >= 1 && level_ <= resources__->proc()->print_level()) {
-      } else if (level_ >= 1) {
+      } else if (level_ >= 1 && level_ < 3) { // TODO for the time being suppressing the level 3 output
         const std::string indent(13+2*level_, ' ');
         const std::string mark = (level_ == 1 ? "o" : (level_ == 2 ? "*" : "-"));
         std::cout << indent << std::left << mark << " " << std::setw(35) << title << std::right << std::setw(13) << std::setprecision(2) << tick() << std::endl;
