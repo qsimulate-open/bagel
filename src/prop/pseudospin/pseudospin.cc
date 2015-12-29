34c3d23df4454b3b03d17a6d0591c85df07c072b
Ryan Dave Reynolds <ryanreynolds2018@u.northwestern.edu>
2015-12-29 01:50:59 -0600

    Bug Fix
    Apparently I was not correctly converting from the MO basis to eigenstate basis for spin operator matrices?  This seems to help somewhat with the pseudospin phase issue.

--- src/prop/pseudospin/pseudospin.cc
+++ src/prop/pseudospin/pseudospin.cc
@@ -255,14 +255,13 @@ void Pseudospin::compute_numerical_hamiltonian(const ZHarrison& zfci, shared_ptr
 
       ZMatrix modensity (2 * norb, 2 * norb);
       modensity.copy_block(0, 0, 2 * norb, 2 * norb, rdmmat);
-
-      ZMatrix aodensity = (*active_coeff * modensity ^ *active_coeff);
+      ZMatrix aodenconj = (*active_coeff * *modensity.get_conjg() ^ *active_coeff);
 
       trev_h_->element(i,j) = aodensity.dot_product(*ao_trev);
       for (int k = 0; k != 3; ++k) {
-        spinop_h_[k]->element(i,j) = aodensity.dot_product(*magnetic_moment[k]);
-        zfci_spin_[k]->element(i,j) = aodensity.dot_product(*ao_spin(k));
-        zfci_orbang_[k]->element(i,j) = aodensity.dot_product(*ao_orbang[k]);
+        spinop_h_[k]->element(i,j) = aodenconj.dot_product(*magnetic_moment[k]);
+        zfci_spin_[k]->element(i,j) = aodenconj.dot_product(*ao_spin(k));
+        zfci_orbang_[k]->element(i,j) = aodenconj.dot_product(*ao_orbang[k]);
       }
     }
   }
