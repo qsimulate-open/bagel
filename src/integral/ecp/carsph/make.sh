a96c0123238088101f2261e706a24f87d9833524
Toru Shiozaki <shiozaki@northwestern.edu>
2016-01-17 07:46:26 -0600

    resolves #65

--- src/integral/ecp/carsph/make.sh
+++ src/integral/ecp/carsph/make.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-
+//
+// BAGEL - Brilliantly Advanced General Electronic Structure Library
 g++ -Wall -Werror -O3 -std=c++11 -fopenmp -lblas -llapack -L/opt/local/lib -lgmp -lmpfr -I$HOME/develop/BAGEL carsph_tab.cc -o tab
 chmod 700 tab
--- src/integral/rys/interpolate/make.sh
+++ src/integral/rys/interpolate/make.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-
+//
+// BAGEL - Brilliantly Advanced General Electronic Structure Library
 #g++ -O0 -c ../_breitroot_*.cc
 #g++ -O0 -c ../_spin2root_*.cc
 
@@ -8,7 +8,15 @@
 #g++ -Ofast -std=c++11 -DSPIN2 -lblas -llapack -lgmp -lmpfr *.cc *.c _breit*.o _spin2*.o -o gen
 
 # for debug
-#g++ -O0 -g -std=c++11 -fopenmp -DDAWSON -lgsl -lblas -llapack -I/usr/local/include -L/opt/local/lib -lgmp -lmpfr -I$HOME/develop/BAGEL *.cc ../_r2root*.cc -o gen
-# for production runs
-g++ -O3 -std=c++11 -fopenmp -DDAWSON -lgsl -lblas -llapack -I/usr/local/include -L/opt/local/lib -lgmp -lmpfr -I$HOME/develop/BAGEL *.cc ../_r2root*.cc -o gen
-chmod 700 gen
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
