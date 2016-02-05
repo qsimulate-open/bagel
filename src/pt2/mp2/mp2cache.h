qsimulate-open/bagel

commit 3df4b8192bdb91f2fc5db5d93509fa8feef26ab5

MP2Cache is generalized so that it could be used with relativistic theories.

src/pt2/mp2/mp2cache.h
@@ -28,30 +28,95 @@
 
 #include <set>
 #include <src/df/dfdistt.h>
+#include <src/df/reldffullt.h>
 
 namespace bagel {
 
-class MP2Cache {
+template <typename DataType>
+class MP2Tag {
+  protected:
+    typename std::conditional<std::is_same<DataType,double>::value,std::array<int,1>,std::array<int,2>>::type tag_;
+
+  public:
+    MP2Tag() { std::fill(tag_.begin(), tag_.end(), -1); }
+    MP2Tag(std::initializer_list<int> o) {
+      assert(o.size() == tag_.size());
+      auto j = tag_.begin();
+      for (auto& i : o)
+        *j++ = i;
+    }
+
+    MP2Tag<DataType>& operator=(const MP2Tag<DataType>& o) { tag_ = o.tag_; return *this; }
+    bool operator<(const MP2Tag<DataType>& o) const {
+      auto j = tag_.begin();
+      for (auto i : o.tag_)
+        if (*j++ != i) return *j++ < i;
+      return false;
+    }
+    bool operator==(const MP2Tag<DataType>& o) const {
+      return !(*this < o) && !(o < *this);
+    }
+    bool invalid() const { return tag_.at(0) == -1; }
+    void wait() const { for (auto& i : tag_) mpi__->wait(i); }
+    bool test() const {
+      bool out = true;
+      for (auto& i : tag_) out &= mpi__->test(i);
+      return out;
+    }
+};
+
+template <typename DataType>
+class MP2Cache_ {
+  protected:
+    using CacheType = typename std::conditional<std::is_same<DataType,double>::value, std::shared_ptr<Matrix>,
+                                                                                      std::pair<std::shared_ptr<Matrix>,std::shared_ptr<Matrix>>>::type;
+    using DFType    = typename std::conditional<std::is_same<DataType,double>::value, DFDistT, RelDFFullT>::type;
+    using MatType   = typename std::conditional<std::is_same<DataType,double>::value, Matrix, ZMatrix>::type;
+
   protected:
     const size_t naux_;
     const size_t nocc_;
     const size_t nvirt_;
 
-    const std::shared_ptr<const DFDistT> fullt_; // aux, virt, occ
-    std::vector<std::vector<std::tuple<int,int,int,int>>> tasks_;
+    const std::shared_ptr<const DFType> fullt_; // aux, virt, occ
+    std::vector<std::vector<std::tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>>> tasks_;
 
     // the data is stored in a map
-    std::map<int, std::shared_ptr<Matrix>> cache_;
+    std::map<int, CacheType> cache_;
     // pair of node and set of integers
     std::vector<std::set<int>> cachetable_;
-    std::vector<int> sendreqs_;
+    std::vector<MP2Tag<DataType>> sendreqs_;
 
     int myrank_;
     int nloop_;
 
+    MP2Tag<DataType> create_cache_and_request_recv(const int i, const int origin);
+    MP2Tag<DataType> request_one(const int i, const int rank) {
+      if (i < 0 || i >= nocc_) return MP2Tag<DataType>();
+      cachetable_[rank].insert(i);
+      MP2Tag<DataType> tag;
+      if (cache_.find(i) == cache_.end() && myrank_ == rank) {
+        const int origin = fullt_->locate(0, i*nvirt_);
+        if (origin == myrank_)
+          cache_[i] = fullt_->get_slice(i*nvirt_, (i+1)*nvirt_).front();
+        else
+          tag = create_cache_and_request_recv(i, origin);
+      }
+      return tag;
+    }
+
+    MP2Tag<DataType> request_send(const int i, const int dest);
+    MP2Tag<DataType> send_one(const int i, const int dest) {
+      // see if "i" is cached at dest
+      if (i < 0 || i >= nocc_ || cachetable_[dest].count(i) || fullt_->locate(0, i*nvirt_) != myrank_)
+        return MP2Tag<DataType>();
+      return request_send(i, dest);
+    }
+
   public:
-    MP2Cache(const int naux, const int nocc, const int nvirt, std::shared_ptr<const DFDistT> fullt,
-             const std::vector<std::vector<std::tuple<int,int,int,int>>>& tasks = std::vector<std::vector<std::tuple<int,int,int,int>>>())
+    MP2Cache_(const int naux, const int nocc, const int nvirt, std::shared_ptr<const DFType> fullt,
+              const std::vector<std::vector<std::tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>>>& tasks
+                  = std::vector<std::vector<std::tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>>>())
      : naux_(naux), nocc_(nocc), nvirt_(nvirt), fullt_(fullt), tasks_(tasks), cachetable_(mpi__->size()), myrank_(mpi__->rank()) {
 
       assert(naux_ == fullt->naux());
@@ -65,23 +130,23 @@ class MP2Cache {
           for (int i = 0, cnt = 0; i < nocc; ++i)
             for (int j = i; j < nocc; ++j, ++cnt)
               if (cnt >= ijdist.start(inode) && cnt < ijdist.start(inode) + ijdist.size(inode))
-                tasks_[inode].push_back(std::make_tuple(j, i, /*mpitags*/-1,-1));
+                tasks_[inode].push_back(std::make_tuple(j, i, MP2Tag<DataType>(),MP2Tag<DataType>()));
           if (tasks_[inode].size() > nmax) nmax = tasks_[inode].size();
         }
         for (auto& i : tasks_) {
           const int n = i.size();
-          for (int j = 0; j != nmax-n; ++j) i.push_back(std::make_tuple(-1,-1,-1,-1));
+          for (int j = 0; j != nmax-n; ++j) i.push_back(std::make_tuple(-1,-1,MP2Tag<DataType>(),MP2Tag<DataType>()));
         }
       }
       nloop_ = tasks_[0].size();
     }
 
-    std::shared_ptr<const Matrix> operator()(const int i) const { return cache_.at(i); }
+    std::shared_ptr<const MatType> operator()(const int i) const;
+
+    const std::tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>& task(const int i) const { return tasks_[myrank_][i]; }
+    const std::vector<std::vector<std::tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>>>& tasks() const { return tasks_; }
 
     int nloop() const { return nloop_; }
-
-    const std::tuple<int,int,int,int>& task(const int i) const { return tasks_[myrank_][i]; }
-    const std::vector<std::vector<std::tuple<int,int,int,int>>>& tasks() const { return tasks_; }
 
     void block(const int nadd, const int ndrop) {
       assert(ndrop < nadd);
@@ -106,62 +171,51 @@ class MP2Cache {
         }
       }
       if (nadd < nloop_) {
-        // issue recv requests
-        auto request_one_ = [&](const int i, const int rank) {
-          if (i < 0 || i >= nocc_) return -1;
-          cachetable_[rank].insert(i);
-          int tag = -1;
-          if (cache_.find(i) == cache_.end() && myrank_ == rank) {
-            const int origin = fullt_->locate(0, i*nvirt_);
-            if (origin == myrank_) {
-              cache_[i] = fullt_->get_slice(i*nvirt_, (i+1)*nvirt_).front();
-            } else {
-              cache_[i] = std::make_shared<Matrix>(naux_, nvirt_, true);
-              tag = mpi__->request_recv(cache_[i]->data(), cache_[i]->size(), origin, myrank_*nocc_+i);
-            }
-          }
-          return tag;
-        };
-
-        // issue send requests
-        auto send_one_ = [&](const int i, const int dest) {
-          // see if "i" is cached at dest
-          if (i < 0 || i >= nocc_ || cachetable_[dest].count(i) || fullt_->locate(0, i*nvirt_) != myrank_)
-            return -1;
-          return mpi__->request_send(fullt_->data() + (i*nvirt_-fullt_->bstart())*naux_, nvirt_*naux_, dest, dest*nocc_+i);
-        };
-
         for (int inode = 0; inode != mpi__->size(); ++inode) {
           if (inode == myrank_) {
             // recieve data from other processes
-            std::get<2>(tasks_[myrank_][nadd]) = request_one_(std::get<0>(tasks_[myrank_][nadd]), myrank_); // receive requests
-            std::get<3>(tasks_[myrank_][nadd]) = request_one_(std::get<1>(tasks_[myrank_][nadd]), myrank_);
+            std::get<2>(tasks_[myrank_][nadd]) = request_one(std::get<0>(tasks_[myrank_][nadd]), myrank_); // receive requests
+            std::get<3>(tasks_[myrank_][nadd]) = request_one(std::get<1>(tasks_[myrank_][nadd]), myrank_);
           } else {
             // send data to other processes
-            const int i = send_one_(std::get<0>(tasks_[inode][nadd]), inode); // send requests
-            if (i >= 0) sendreqs_.push_back(i);
-            request_one_(std::get<0>(tasks_[inode][nadd]), inode); // update cachetable_
-            const int j = send_one_(std::get<1>(tasks_[inode][nadd]), inode); // send requests
-            if (j >= 0) sendreqs_.push_back(j);
-            request_one_(std::get<1>(tasks_[inode][nadd]), inode);
+            const MP2Tag<DataType> i = send_one(std::get<0>(tasks_[inode][nadd]), inode); // send requests
+            if (!i.invalid()) sendreqs_.push_back(i);
+            request_one(std::get<0>(tasks_[inode][nadd]), inode); // update cachetable_
+            const MP2Tag<DataType> j = send_one(std::get<1>(tasks_[inode][nadd]), inode); // send requests
+            if (!j.invalid()) sendreqs_.push_back(j);
+            request_one(std::get<1>(tasks_[inode][nadd]), inode);
           }
         }
       }
     }
 
     void data_wait(const int n) const {
-      const int ti = std::get<2>(task(n));
-      const int tj = std::get<3>(task(n));
-      if (ti >= 0) mpi__->wait(ti);
-      if (tj >= 0) mpi__->wait(tj);
+      const MP2Tag<DataType> ti = std::get<2>(task(n));
+      const MP2Tag<DataType> tj = std::get<3>(task(n));
+      if (!ti.invalid()) ti.wait();
+      if (!tj.invalid()) tj.wait();
     }
 
     void wait() const {
       for (auto& i : sendreqs_)
-        mpi__->wait(i);
+        i.wait();
     }
 
 };
+
+template<> MP2Tag<double> MP2Cache_<double>::create_cache_and_request_recv(const int i, const int origin);
+template<> MP2Tag<double> MP2Cache_<double>::request_send(const int i, const int dest);
+template<> std::shared_ptr<const Matrix> MP2Cache_<double>::operator()(const int i) const;
+
+template<> MP2Tag<std::complex<double>> MP2Cache_<std::complex<double>>::create_cache_and_request_recv(const int i, const int origin);
+template<> MP2Tag<std::complex<double>> MP2Cache_<std::complex<double>>::request_send(const int i, const int dest);
+template<> std::shared_ptr<const ZMatrix> MP2Cache_<std::complex<double>>::operator()(const int i) const;
+
+extern template class MP2Cache_<double>;
+extern template class MP2Cache_<std::complex<double>>;
+
+using MP2Cache = MP2Cache_<double>;
+using RelMP2Cache = MP2Cache_<std::complex<double>>;
 
 }
 

src/df/Makefile.am
@@ -1,3 +1,3 @@
 lib_LTLIBRARIES = libbagel_df.la
-libbagel_df_la_SOURCES = dfblock.cc df.cc dfdistt.cc paralleldf.cc complexdf.cc complexdf_base.cc reldf.cc reldfhalf.cc reldffull.cc relcdmatrix.cc breit2index.cc
+libbagel_df_la_SOURCES = dfblock.cc df.cc dfdistt.cc paralleldf.cc complexdf.cc complexdf_base.cc reldf.cc reldfhalf.cc reldffull.cc reldffullt.cc relcdmatrix.cc breit2index.cc
 AM_CXXFLAGS=-I$(top_srcdir)

src/df/reldffullt.cc
@@ -1,0 +1,47 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: reldffullt.cc
+// Copyright (C) 2015 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+
+#include <src/df/reldffullt.h>
+
+using namespace std;
+using namespace bagel;
+
+RelDFFullT::RelDFFullT(shared_ptr<const RelDFFull> full, shared_ptr<const StaticDist> dist) {
+  dffull_[0] = make_shared<DFDistT>(full->get_real(), dist);
+  dffull_[1] = make_shared<DFDistT>(full->get_imag(), dist);
+}
+
+
+void RelDFFullT::discard_df() {
+  dffull_[0]->discard_df();
+  dffull_[1]->discard_df();
+}
+
+
+vector<pair<shared_ptr<Matrix>,shared_ptr<Matrix>>> RelDFFullT::get_slice(const int start, const int end) const {
+  assert(dffull_[0]->nblocks() == 1 && dffull_[1]->nblocks() == 1);
+  return {make_pair(dffull_[0]->get_slice(start, end).at(0), dffull_[1]->get_slice(start, end).at(0))};
+}

src/df/reldffullt.h
@@ -1,0 +1,56 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: reldffullt.h
+// Copyright (C) 2015 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef __SRC_DF_RELDFFULLT_H
+#define __SRC_DF_RELDFFULLT_H
+
+#include <src/df/dfdistt.h>
+#include <src/df/reldffull.h>
+
+namespace bagel {
+
+class RelDFFullT {
+  protected:
+    std::array<std::shared_ptr<DFDistT>,2> dffull_;
+
+  public:
+    RelDFFullT(std::shared_ptr<const RelDFFull> full, std::shared_ptr<const StaticDist> dist = nullptr);
+
+    const double* data(const int i) const { return dffull_.at(i)->data(); }
+
+    // to make this class compatible with DFDistT
+    int naux()   const { assert(dffull_[0]->naux() == dffull_[1]->naux());     return dffull_[0]->naux(); }
+    int bstart() const { assert(dffull_[0]->bstart() == dffull_[1]->bstart()); return dffull_[0]->bstart(); }
+    void discard_df();
+    std::vector<std::pair<std::shared_ptr<Matrix>,std::shared_ptr<Matrix>>> get_slice(const int start, const int end) const;
+
+    int locate(const size_t i, const size_t n) const { assert(dffull_[0]->locate(i,n) == dffull_[1]->locate(i,n)); return dffull_[0]->locate(i,n); }
+
+    static int nblocks() { return 1; }
+};
+
+}
+
+#endif

src/pt2/Makefile.am
@@ -1,5 +1,5 @@
 AUTOMAKE_OPTIONS = subdir-objects
 lib_LTLIBRARIES = libbagel_pt2.la
-libbagel_pt2_la_SOURCES = mp2/mp2.cc mp2/mp2grad.cc mp2/f12int.cc mp2/f12mat.cc mp2/f12int4.cc nevpt2/nevpt2.cc \
+libbagel_pt2_la_SOURCES = mp2/mp2.cc mp2/mp2grad.cc mp2/mp2cache.cc mp2/f12int.cc mp2/f12mat.cc mp2/f12int4.cc nevpt2/nevpt2.cc \
                           dmp2/dmp2.cc dmp2/dmp2grad.cc
 AM_CXXFLAGS=-I$(top_srcdir)

src/pt2/mp2/mp2accum.h
@@ -33,22 +33,23 @@ namespace bagel {
 
 class MP2Accum {
   protected:
+    using Tag = MP2Tag<double>;
     const size_t naux_;
     const size_t nocc_;
     const size_t nvirt_;
 
     const std::shared_ptr<DFDistT> fullt_; // aux, virt, occ
-    std::vector<std::vector<std::tuple<int,int,int,int>>> tasks_;
+    std::vector<std::vector<std::tuple<int,int,Tag,Tag>>> tasks_;
 
-    std::map<int, std::shared_ptr<const Matrix>> sendreqs_; // mpi tag and buffer
-    std::map<int, std::pair<std::shared_ptr<Matrix>,int>> recvreqs_; // mpi tag, buffer and offset
+    std::map<Tag, std::shared_ptr<const Matrix>> sendreqs_; // mpi tag and buffer
+    std::map<Tag, std::pair<std::shared_ptr<Matrix>,int>> recvreqs_; // mpi tag, buffer and offset
 
     const int myrank_;
     const int nloop_;
 
   public:
     MP2Accum(const int naux, const int nocc, const int nvirt, std::shared_ptr<DFDistT> fullt,
-             const std::vector<std::vector<std::tuple<int,int,int,int>>>& tasks)
+             const std::vector<std::vector<std::tuple<int,int,Tag,Tag>>>& tasks)
      : naux_(naux), nocc_(nocc), nvirt_(nvirt), fullt_(fullt), tasks_(tasks), myrank_(mpi__->rank()), nloop_(tasks_[0].size()) {
       assert(naux_ == fullt->naux() && nvirt_ == fullt->nindex1() && nocc_ == fullt->nindex2());
     }
@@ -57,13 +58,13 @@ class MP2Accum {
     void accumulate(const int n, std::shared_ptr<const Matrix> send) {
       // first scan sendreqs and recvreqs and drop matrices that have been sent/received
       for (auto i = sendreqs_.begin(); i != sendreqs_.end(); ) {
-        if (mpi__->test(i->first))
+        if (i->first.test())
           sendreqs_.erase(i++);
         else
           ++i;
       }
       for (auto i = recvreqs_.begin(); i != recvreqs_.end(); ) {
-        if (mpi__->test(i->first)) {
+        if (i->first.test()) {
           blas::ax_plus_y_n(1.0, i->second.first->data(), i->second.first->size(), fullt_->data()+fullt_->offset(0, i->second.second*nvirt_));
           recvreqs_.erase(i++);
         } else {
@@ -81,7 +82,7 @@ class MP2Accum {
         if (dest == myrank_) {
           blas::ax_plus_y_n(1.0, send->data(), send->size(), fullt_->data()+fullt_->offset(0, i*nvirt_));
         } else {
-          sendreqs_.emplace(mpi__->request_send(send->data(), send->size(), dest, tag(dest, myrank_)), send);
+          sendreqs_.emplace(Tag{mpi__->request_send(send->data(), send->size(), dest, tag(dest, myrank_))}, send);
         }
       };
 
@@ -90,7 +91,7 @@ class MP2Accum {
         if (i < 0 || i >= nocc_ || fullt_->locate(0, i*nvirt_) != myrank_)
           return;
         auto buf = std::make_shared<Matrix>(naux_, nvirt_);
-        recvreqs_.emplace(mpi__->request_recv(buf->data(), buf->size(), origin, tag(myrank_, origin)), std::make_pair(buf, i));
+        recvreqs_.emplace(Tag{mpi__->request_recv(buf->data(), buf->size(), origin, tag(myrank_, origin))}, std::make_pair(buf, i));
       };
 
       // send, or locally accumulate
@@ -106,11 +107,11 @@ class MP2Accum {
 
     void wait() {
       for (auto& i : recvreqs_) {
-        mpi__->wait(i.first);
+        i.first.wait();
         blas::ax_plus_y_n(1.0, i.second.first->data(), i.second.first->size(), fullt_->data()+fullt_->offset(0, i.second.second*nvirt_));
       }
       for (auto& i : sendreqs_)
-        mpi__->wait(i.first);
+        i.first.wait();
       recvreqs_.clear();
       sendreqs_.clear();
     }

src/pt2/mp2/mp2cache.cc
@@ -1,0 +1,66 @@
+//
+// BAGEL - Parallel electron correlation program.
+// Filename: mp2cache.h
+// Copyright (C) 2015 Toru Shiozaki
+//
+// Author: Toru Shiozaki <shiozaki@northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// The BAGEL package is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// The BAGEL package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the BAGEL package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <src/pt2/mp2/mp2cache.h>
+
+using namespace bagel;
+
+template<>
+MP2Tag<double> MP2Cache_<double>::create_cache_and_request_recv(const int i, const int origin) {
+  cache_[i] = std::make_shared<Matrix>(naux_, nvirt_, true);
+  return MP2Tag<double>{mpi__->request_recv(cache_[i]->data(), cache_[i]->size(), origin, myrank_*nocc_+i)};
+}
+
+template<>
+MP2Tag<std::complex<double>> MP2Cache_<std::complex<double>>::create_cache_and_request_recv(const int i, const int origin) {
+  cache_[i] = {std::make_shared<Matrix>(naux_, nvirt_, true), std::make_shared<Matrix>(naux_, nvirt_, true)};
+  return MP2Tag<std::complex<double>>{mpi__->request_recv(cache_[i].first->data(),  cache_[i].first->size(),  origin, myrank_*nocc_*2+i*2),
+                                      mpi__->request_recv(cache_[i].second->data(), cache_[i].second->size(), origin, myrank_*nocc_*2+i*2+1)};
+}
+
+template<>
+MP2Tag<double> MP2Cache_<double>::request_send(const int i, const int dest) {
+  return MP2Tag<double>{mpi__->request_send(fullt_->data() + (i*nvirt_-fullt_->bstart())*naux_, nvirt_*naux_, dest, dest*nocc_+i)};
+}
+
+template<>
+MP2Tag<std::complex<double>> MP2Cache_<std::complex<double>>::request_send(const int i, const int dest) {
+  return MP2Tag<std::complex<double>>{mpi__->request_send(fullt_->data(0) + (i*nvirt_-fullt_->bstart())*naux_, nvirt_*naux_, dest, dest*nocc_*2+i*2),
+                                      mpi__->request_send(fullt_->data(1) + (i*nvirt_-fullt_->bstart())*naux_, nvirt_*naux_, dest, dest*nocc_*2+i*2+1)};
+}
+
+template<>
+std::shared_ptr<const Matrix> MP2Cache_<double>::operator()(const int i) const {
+  return cache_.at(i);
+}
+
+template<>
+std::shared_ptr<const ZMatrix> MP2Cache_<std::complex<double>>::operator()(const int i) const {
+  return std::make_shared<ZMatrix>(*cache_.at(i).first, *cache_.at(i).second);
+}
+
+/// specialization ///
+template class MP2Cache_<double>;
+template class MP2Cache_<std::complex<double>>;

src/pt2/nevpt2/nevpt2.cc
@@ -25,7 +25,6 @@
 
 #include <src/pt2/nevpt2/nevpt2.h>
 #include <src/pt2/mp2/mp2cache.h>
-#include <src/df/dfdistt.h>
 #include <src/scf/hf/fock.h>
 #include <src/scf/dhf/dfock.h>
 #include <src/mat1e/rel/relhcore.h>
@@ -266,7 +265,8 @@ void NEVPT2_<DataType>::compute() {
 
   /////////////////////////////////////////////////////////////////////////////////////
   // make a list of static distribution
-  vector<vector<tuple<int,int,int,int>>> tasks(mpi__->size());
+  vector<vector<tuple<int,int,MP2Tag<DataType>,MP2Tag<DataType>>>> tasks(mpi__->size());
+  MP2Tag<DataType> dum;
   // distribution of closed-closed
   if (nclosed_) {
     StaticDist ijdist(nclosed_*(nclosed_+1)/2, mpi__->size());
@@ -274,7 +274,7 @@ void NEVPT2_<DataType>::compute() {
       for (int i = 0, cnt = 0; i < nclosed_; ++i)
         for (int j = i; j < nclosed_; ++j, ++cnt)
           if (cnt >= ijdist.start(inode) && cnt < ijdist.start(inode) + ijdist.size(inode))
-            tasks[inode].push_back(make_tuple(j, i, /*mpitags*/-1,-1));
+            tasks[inode].push_back(make_tuple(j, i, /*mpitags*/dum,dum));
     }
   }
   // distribution of virt-virt (cheap as both involve active indices)
@@ -284,7 +284,7 @@ void NEVPT2_<DataType>::compute() {
       for (int i = 0, cnt = 0; i < nvirt_; ++i)
         for (int j = i; j < nvirt_; ++j, ++cnt)
           if (cnt >= ijdist.start(inode) && cnt < ijdist.start(inode) + ijdist.size(inode))
-            tasks[inode].push_back(make_tuple(j+nclosed_+nact_, i+nclosed_+nact_, /*mpitags*/-1,-1));
+            tasks[inode].push_back(make_tuple(j+nclosed_+nact_, i+nclosed_+nact_, /*mpitags*/dum,dum));
     }
   }
   // distribution of closed (sort of cheap)
@@ -293,7 +293,7 @@ void NEVPT2_<DataType>::compute() {
     for (int inode = 0; inode != mpi__->size(); ++inode) {
       for (int i = 0; i < nclosed_; ++i)
         if (i >= ijdist.start(inode) && i < ijdist.start(inode) + ijdist.size(inode))
-          tasks[inode].push_back(make_tuple(i, -1, /*mpitags*/-1,-1));
+          tasks[inode].push_back(make_tuple(i, -1, /*mpitags*/dum,dum));
     }
   }
   // distribution of virt for S_r(-1) (sort of cheap)
@@ -302,7 +302,7 @@ void NEVPT2_<DataType>::compute() {
     for (int inode = 0; inode != mpi__->size(); ++inode) {
       for (int i = 0; i < nvirt_; ++i)
         if (i >= ijdist.start(inode) && i < ijdist.start(inode) + ijdist.size(inode))
-          tasks[inode].push_back(make_tuple(i+nclosed_+nact_, -1, /*mpitags*/-1,-1));
+          tasks[inode].push_back(make_tuple(i+nclosed_+nact_, -1, /*mpitags*/dum,dum));
     }
   }
   {
@@ -311,7 +311,7 @@ void NEVPT2_<DataType>::compute() {
       if (nmax < i.size()) nmax = i.size();
     for (auto& i : tasks) {
       const int n = i.size();
-      for (int j = 0; j != nmax-n; ++j) i.push_back(make_tuple(-1,-1,-1,-1));
+      for (int j = 0; j != nmax-n; ++j) i.push_back(make_tuple(-1,-1,dum,dum));
     }
   }
 

commit 4a0aaa2a05139f42951c3b608022d443e78414ba

resolves #68. Stupid bug introduced in 3df4b8192bdb91f2fc5db5d93509fa8feef26ab5

src/pt2/mp2/mp2cache.h
@@ -48,8 +48,10 @@ class MP2Tag {
     MP2Tag<DataType>& operator=(const MP2Tag<DataType>& o) { tag_ = o.tag_; return *this; }
     bool operator<(const MP2Tag<DataType>& o) const {
       auto j = tag_.begin();
-      for (auto i : o.tag_)
-        if (*j++ != i) return *j++ < i;
+      for (auto i : o.tag_) {
+        if (*j != i) return *j < i;
+        ++j;
+      }
       return false;
     }
     bool operator==(const MP2Tag<DataType>& o) const {