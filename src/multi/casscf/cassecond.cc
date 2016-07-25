76b592910688da5b9264556cde61072587dce4e6
Toru Shiozaki <shiozaki@northwestern.edu>
2016-07-25 12:28:39 -0500

    so that the code works without closed orbitals (unverified)

--- src/multi/casscf/cassecond.cc
+++ src/multi/casscf/cassecond.cc
@@ -30,13 +30,14 @@ using namespace std;
 using namespace bagel;
 
 void CASSecond::compute() {
+  assert(nvirt_ && nact_);
   Timer timer;
 
   mute_stdcout();
   for (int iter = 0; iter != max_iter_; ++iter) {
 
     // first perform CASCI to obtain RDMs
-    if (nact_) {
+    {
       if (iter) fci_->update(coeff_);
       Timer fci_time(0);
       fci_->compute();
@@ -44,6 +45,7 @@ void CASSecond::compute() {
       auto natorb = fci_->natorb_convert();
       coeff_ = update_coeff(coeff_, natorb.first);
       fci_time.tick_print("FCI and RDMs");
+      energy_ = fci_->energy();
     }
 
     // coeff spaces and RDM1
@@ -57,43 +59,50 @@ void CASSecond::compute() {
     shared_ptr<const Matrix> cfockao = nclosed_ ? make_shared<Fock<1>>(geom_, hcore_, nullptr, ccoeff, /*store*/true, /*rhf*/true) : hcore_;
     shared_ptr<const Matrix> cfock = make_shared<Matrix>(*coeff_ % *cfockao * *coeff_);
     shared_ptr<const Matrix> afock;
-    if (nact_) {
+    {
       Matrix dkl = rdm1;
       dkl.sqrt();
       dkl.scale(1.0/sqrt(2.0));
       auto afockao = make_shared<Fock<1>>(geom_, hcore_, nullptr, acoeff * dkl, /*store*/false, /*rhf*/true);
       afock = make_shared<Matrix>(*coeff_ % (*afockao - *hcore_) * *coeff_);
-    } else {
-      afock = cfock->clone();
     }
-    shared_ptr<const Qvec> qxr;
-    if (nact_)
-      qxr = make_shared<const Qvec>(coeff_->mdim(), nact_, coeff_, nclosed_, fci_, fci_->rdm2_av());
+    shared_ptr<const Qvec> qxr = make_shared<Qvec>(coeff_->mdim(), nact_, coeff_, nclosed_, fci_, fci_->rdm2_av());
 
     auto grad = make_shared<RotFile>(nclosed_, nact_, nvirt_);
     grad_vc(cfock, afock, grad);
     grad_va(cfock, qxr, grad);
     grad_ca(cfock, afock, qxr, grad);
 
+    // check gradient and break if converged
+    const double gradient = grad->rms();
+    resume_stdcout();
+    print_iteration(iter, 0, 0, energy_, gradient, timer.tick());
+    mute_stdcout();
+    if (gradient < thresh_) {
+      resume_stdcout();
+      cout << endl << "    * Second-order optimization converged. *   " << endl << endl;
+      mute_stdcout();
+      break;
+    }
+
     // half-transformed integrals (with JJ)
     shared_ptr<const DFHalfDist> half = nclosed_ ? dynamic_pointer_cast<const Fock<1>>(cfockao)->half()->apply_J() : nullptr;
     shared_ptr<const DFHalfDist> halfa = fci_->jop()->mo2e_1ext()->apply_JJ();
 
     // fock submatrices
-    shared_ptr<const Matrix> fccc = cfock->get_submatrix(0, 0, nclosed_, nclosed_);
-    shared_ptr<const Matrix> facc = afock->get_submatrix(0, 0, nclosed_, nclosed_);
-    shared_ptr<const Matrix> fcca = cfock->get_submatrix(0, nclosed_, nclosed_, nact_);
-    shared_ptr<const Matrix> faca = afock->get_submatrix(0, nclosed_, nclosed_, nact_);
     shared_ptr<const Matrix> fcaa = cfock->get_submatrix(nclosed_, nclosed_, nact_, nact_);
     shared_ptr<const Matrix> faaa = afock->get_submatrix(nclosed_, nclosed_, nact_, nact_);
-    shared_ptr<const Matrix> fcvc = cfock->get_submatrix(nocc_, 0, nvirt_, nclosed_);
-    shared_ptr<const Matrix> favc = afock->get_submatrix(nocc_, 0, nvirt_, nclosed_);
     shared_ptr<const Matrix> fcva = cfock->get_submatrix(nocc_, nclosed_, nvirt_, nact_);
     shared_ptr<const Matrix> fava = afock->get_submatrix(nocc_, nclosed_, nvirt_, nact_);
     shared_ptr<const Matrix> fcvv = cfock->get_submatrix(nocc_, nocc_, nvirt_, nvirt_);
     shared_ptr<const Matrix> favv = afock->get_submatrix(nocc_, nocc_, nvirt_, nvirt_);
+    shared_ptr<const Matrix> fccc = nclosed_ ? cfock->get_submatrix(0, 0, nclosed_, nclosed_) : nullptr;
+    shared_ptr<const Matrix> facc = nclosed_ ? afock->get_submatrix(0, 0, nclosed_, nclosed_) : nullptr;
+    shared_ptr<const Matrix> fcca = nclosed_ ? cfock->get_submatrix(0, nclosed_, nclosed_, nact_) : nullptr;
+    shared_ptr<const Matrix> faca = nclosed_ ? afock->get_submatrix(0, nclosed_, nclosed_, nact_) : nullptr;
+    shared_ptr<const Matrix> fcvc = nclosed_ ? cfock->get_submatrix(nocc_, 0, nvirt_, nclosed_) : nullptr;
+    shared_ptr<const Matrix> favc = nclosed_ ? afock->get_submatrix(nocc_, 0, nvirt_, nclosed_) : nullptr;
 
-    // create
     for (int miter = 0; miter != max_micro_iter_; ++miter) {
       Timer mtimer;
       // trial vector
@@ -102,9 +111,9 @@ trot->ax_plus_y_vc(1.0, *grad->vc_mat());
 trot->ax_plus_y_va(1.0, *grad->va_mat());
 trot->ax_plus_y_ca(1.0, *grad->ca_mat());
 trot->print();
-      shared_ptr<const Matrix> ca = trot->ca_mat();
       shared_ptr<const Matrix> va = trot->va_mat();
-      shared_ptr<const Matrix> vc = trot->vc_mat();
+      shared_ptr<const Matrix> ca = nclosed_ ? trot->ca_mat() : nullptr;
+      shared_ptr<const Matrix> vc = nclosed_ ? trot->vc_mat() : nullptr;
       // sigma vector
       auto sigma = grad->clone();
 
@@ -119,7 +128,7 @@ trot->print();
       };
 
       // g(t_vc) operator and g(t_ac) operator
-      {
+      if (nclosed_) {
         const Matrix tcoeff = vcoeff * *vc + acoeff * *ca->transpose();
         auto halft = geom_->df()->compute_half_transform(tcoeff);
         const Matrix gt = *compute_gd(halft, half, ccoeff);
@@ -129,9 +138,9 @@ trot->print();
         sigma->ax_plus_y_ca(-16.0, ccoeff % gt * acoeff * rdm1);
       }
       // g(t_va - t_ca)
-      const Matrix tcoeff = vcoeff * *va - ccoeff * *ca;
+      const Matrix tcoeff = nclosed_ ? (vcoeff * *va - ccoeff * *ca) : vcoeff * *va;
       shared_ptr<const DFHalfDist> halfta = geom_->df()->compute_half_transform(tcoeff);
-      {
+      if (nclosed_) {
         shared_ptr<DFHalfDist> halftad = halfta->copy();
         halftad->rotate_occ(make_shared<Matrix>(rdm1));
         const Matrix gt = *compute_gd(halftad, halfa, acoeff);
@@ -140,17 +149,19 @@ trot->print();
       }
       // terms with Qvec
       {
-        shared_ptr<const Matrix> qca = qxr->cut(0, nclosed_);
         shared_ptr<const Matrix> qaa = qxr->cut(nclosed_, nocc_);
         shared_ptr<const Matrix> qva = qxr->cut(nocc_, nocc_+nvirt_);
         sigma->ax_plus_y_va(-2.0, *va ^ *qaa);
         sigma->ax_plus_y_va(-2.0, *va * *qaa);
-        sigma->ax_plus_y_vc(-2.0, *va ^ *qca);
-        sigma->ax_plus_y_va(-2.0, *vc * *qca);
-        sigma->ax_plus_y_ca(-2.0, *vc % *qva);
-        sigma->ax_plus_y_vc(-2.0, *qva ^ *ca);
-        sigma->ax_plus_y_ca(-2.0, *ca ^ *qaa);
-        sigma->ax_plus_y_ca(-2.0, *ca * *qaa);
+        if (nclosed_) {
+          shared_ptr<const Matrix> qca = qxr->cut(0, nclosed_);
+          sigma->ax_plus_y_vc(-2.0, *va ^ *qca);
+          sigma->ax_plus_y_va(-2.0, *vc * *qca);
+          sigma->ax_plus_y_ca(-2.0, *vc % *qva);
+          sigma->ax_plus_y_vc(-2.0, *qva ^ *ca);
+          sigma->ax_plus_y_ca(-2.0, *ca ^ *qaa);
+          sigma->ax_plus_y_ca(-2.0, *ca * *qaa);
+        }
       }
       // compute Q' and Q''
       {
@@ -164,32 +175,35 @@ trot->print();
         shared_ptr<const Matrix> qpp = halfta->form_2index(fullaaD, 1.0);
 
         sigma->ax_plus_y_va( 4.0, vcoeff % (*qp + *qpp));
-        sigma->ax_plus_y_ca(-4.0, ccoeff % (*qp + *qpp));
+        if (nclosed_)
+          sigma->ax_plus_y_ca(-4.0, ccoeff % (*qp + *qpp));
       }
 
       // next 1-electron contribution...
       {
-        sigma->ax_plus_y_ca( 8.0, *ca * (*fcaa + *faaa));
-        sigma->ax_plus_y_ca( 8.0, *vc % (*fcva + *fava));
-        sigma->ax_plus_y_vc(-8.0, *vc * (*fccc + *facc));
-        sigma->ax_plus_y_va(-4.0, *vc * (*fcca + *faca));
-        sigma->ax_plus_y_vc(-4.0, *va ^ (*fcca + *faca));
-        sigma->ax_plus_y_ca(-2.0, *ca * (rdm1 * *fcaa + *fcaa * rdm1));
-        sigma->ax_plus_y_va(-2.0, *va * (rdm1 * *fcaa + *fcaa * rdm1));
-        sigma->ax_plus_y_vc( 8.0, (*fcvv + *favv) * *vc);
-        sigma->ax_plus_y_ca(-8.0, (*fccc + *facc) * *ca);
-        sigma->ax_plus_y_va( 4.0, (*fcvc + *favc) * *ca);
-        sigma->ax_plus_y_ca( 4.0, (*fcvc + *favc) % *va);
-        sigma->ax_plus_y_vc( 4.0, (*fcva + *fava) ^ *ca);
-        sigma->ax_plus_y_vc( 4.0, (*fcva + *fava) ^ *ca);
-        sigma->ax_plus_y_ca( 4.0, *fccc * *ca * rdm1);
         sigma->ax_plus_y_va( 4.0, *fcvv * *va * rdm1);
-        sigma->ax_plus_y_ca(-4.0, *fcvc % *va * rdm1);
-        sigma->ax_plus_y_va(-4.0, *fcvc * *ca * rdm1);
-        sigma->ax_plus_y_vc(-2.0, *fcva * rdm1 ^ *ca);
-        sigma->ax_plus_y_vc(-2.0, *va * rdm1 ^ *fcca);
-        sigma->ax_plus_y_ca(-2.0, *vc % *fcva * rdm1);
-        sigma->ax_plus_y_va(-2.0, *vc * *fcca * rdm1);
+        sigma->ax_plus_y_va(-2.0, *va * (rdm1 * *fcaa + *fcaa * rdm1));
+        if (nclosed_) {
+          sigma->ax_plus_y_ca( 8.0, *ca * (*fcaa + *faaa));
+          sigma->ax_plus_y_ca( 8.0, *vc % (*fcva + *fava));
+          sigma->ax_plus_y_vc(-8.0, *vc * (*fccc + *facc));
+          sigma->ax_plus_y_va(-4.0, *vc * (*fcca + *faca));
+          sigma->ax_plus_y_vc(-4.0, *va ^ (*fcca + *faca));
+          sigma->ax_plus_y_ca(-2.0, *ca * (rdm1 * *fcaa + *fcaa * rdm1));
+          sigma->ax_plus_y_vc( 8.0, (*fcvv + *favv) * *vc);
+          sigma->ax_plus_y_ca(-8.0, (*fccc + *facc) * *ca);
+          sigma->ax_plus_y_va( 4.0, (*fcvc + *favc) * *ca);
+          sigma->ax_plus_y_ca( 4.0, (*fcvc + *favc) % *va);
+          sigma->ax_plus_y_vc( 4.0, (*fcva + *fava) ^ *ca);
+          sigma->ax_plus_y_vc( 4.0, (*fcva + *fava) ^ *ca);
+          sigma->ax_plus_y_ca( 4.0, *fccc * *ca * rdm1);
+          sigma->ax_plus_y_ca(-4.0, *fcvc % *va * rdm1);
+          sigma->ax_plus_y_va(-4.0, *fcvc * *ca * rdm1);
+          sigma->ax_plus_y_vc(-2.0, *fcva * rdm1 ^ *ca);
+          sigma->ax_plus_y_vc(-2.0, *va * rdm1 ^ *fcca);
+          sigma->ax_plus_y_ca(-2.0, *vc % *fcva * rdm1);
+          sigma->ax_plus_y_va(-2.0, *vc * *fcca * rdm1);
+        }
       }
 
       sigma->scale(0.5);
@@ -200,19 +214,10 @@ break;
     }
 
     resume_stdcout();
-const double gradient = 0.0;
-energy_ = {0.0, 0.0};
-    print_iteration(iter, 0, 0, energy_, gradient, timer.tick());
-
-    if (gradient < thresh_) {
-      cout << endl << "    * Second-order optimization converged. *   " << endl << endl;
-      mute_stdcout();
-      break;
-    }
-
     if (iter == max_iter_-1)
       cout << endl << "    * Max iteration reached during the second optimization. *     " << endl << endl;
     mute_stdcout();
+break;
   }
   resume_stdcout();
 
