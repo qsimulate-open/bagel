f4ff7d8dfa6ff7deb116538d15550a1c709fabb5
Toru Shiozaki <shiozaki@northwestern.edu>
2016-12-15 17:03:14 -0600

    Presumably this fixes #84 but needs testing

--- src/smith/caspt2/CASPT2.cc
+++ src/smith/caspt2/CASPT2.cc
@@ -479,7 +479,7 @@ void CASPT2::CASPT2::solve_deriv() {
       for (int jst = 0; jst != nstates_; ++jst) {
         Matrix op(*fock * wmn(jst, ist));
         if (ist == jst)
-          op += *gdc * (1.0/nstates_);
+          op += *gdc * (1.0/nstates_) * 0.5;
         for (int i = 0; i != nact; ++i)
           for (int j = 0; j != nact; ++j)
             ci_deriv_->data(jst)->ax_plus_y(2.0*op(j,i), deriv->data(j+i*nact));
