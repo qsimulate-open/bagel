94bd01a23c1652e512ebb21a16b1d3fc8950c442
Jae Woo Park <triplepjw@gmail.com>
2017-01-16 16:24:23 -0600

    very minor

--- src/main.cc
+++ src/main.cc
@@ -81,7 +81,13 @@ int main(int argc, char** argv) {
         shared_ptr<Reference> ptr;
         archive >> ptr;
         ref = shared_ptr<Reference>(ptr);
-        geom = ref->geom();
+        if (itree->get<bool>("continue_geom", true)) {
+          cout << endl << "  Using the geometry in the archive file " << endl << endl;
+          geom = ref->geom();
+        } else {
+          cout << endl << "  Using the coefficient projected to the input geometry " << endl << endl;
+          ref = ref->project_coeff(geom);
+        }
         if (itree->get<bool>("extract_average_rdms", false)) {
           vector<int> rdm_states = itree->get_vector<int>("rdm_state");
           ref = ref->extract_average_rdm(rdm_states);
