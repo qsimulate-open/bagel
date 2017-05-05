f62f09ff20af6dee29067a5ed767abbf0c24093e
Toru Shiozaki <shiozaki@northwestern.edu>
2017-05-05 12:00:20 -0500

    minor

--- src/util/io/moldenout.cc
+++ src/util/io/moldenout.cc
@@ -111,9 +111,8 @@ void MoldenOut::write_mos() {
     ofs_ << endl;
   }
   ofs_ << endl;
-  if (is_spherical) ofs_ << "[5D]" << endl;
-  if (is_spherical) ofs_ << "[7F]" <<endl;
-  if (is_spherical) ofs_ << "[9G]" <<endl;
+  if (is_spherical)
+    ofs_ << "[5D]" << endl << "[7F]" << endl << "[9G]" << endl;
   ofs_ << "[MO]" << endl;
   const int num_mos = ref_->coeff()->mdim();
   int nocc = ref_->nclosed();
