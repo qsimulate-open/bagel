qsimulate-open/bagel

commit d96742e09cfc5a21e94dfaac075a9e681588299b

CASPT2 code generated with new SMITH3 (accelerates CI derivative part similarly to MS-CASPT2)

src/smith/caspt2/CASPT2_gamma.cc
@@ -26,8 +26,7 @@
 #include <bagel_config.h>
 #ifdef COMPILE_SMITH
 #include <src/smith/caspt2/CASPT2.h>
-#include <src/smith/caspt2/CASPT2_tasks1.h>
-#include <src/smith/caspt2/CASPT2_tasks2.h>
+#include <src/smith/caspt2/CASPT2_tasks.h>
 
 using namespace std;
 using namespace bagel;
@@ -187,22 +186,22 @@ shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma32_() {
   return make_shared<FutureTensor>(*Gamma32, task16);
 }
 
-shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma34_() {
-  vector<IndexRange> Gamma34_index = {active_, active_, active_, active_};
-  auto Gamma34 = make_shared<Tensor>(Gamma34_index);
-  array<shared_ptr<const IndexRange>,3> pindex = {{rclosed_, ractive_, rvirt_}};
-  auto tensor17 = vector<shared_ptr<Tensor>>{Gamma34, rdm1_, rdm2_, rdm3_, f1_};
-  auto task17 = make_shared<Task17>(tensor17, pindex);
-  return make_shared<FutureTensor>(*Gamma34, task17);
-}
-
 shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma35_() {
   vector<IndexRange> Gamma35_index = {active_, active_, active_, active_};
   auto Gamma35 = make_shared<Tensor>(Gamma35_index);
   array<shared_ptr<const IndexRange>,3> pindex = {{rclosed_, ractive_, rvirt_}};
-  auto tensor18 = vector<shared_ptr<Tensor>>{Gamma35, rdm1_, rdm2_};
+  auto tensor17 = vector<shared_ptr<Tensor>>{Gamma35, rdm1_, rdm2_};
+  auto task17 = make_shared<Task17>(tensor17, pindex);
+  return make_shared<FutureTensor>(*Gamma35, task17);
+}
+
+shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma34_() {
+  vector<IndexRange> Gamma34_index = {active_, active_, active_, active_};
+  auto Gamma34 = make_shared<Tensor>(Gamma34_index);
+  array<shared_ptr<const IndexRange>,3> pindex = {{rclosed_, ractive_, rvirt_}};
+  auto tensor18 = vector<shared_ptr<Tensor>>{Gamma34, rdm1_, rdm2_, rdm3_, f1_};
   auto task18 = make_shared<Task18>(tensor18, pindex);
-  return make_shared<FutureTensor>(*Gamma35, task18);
+  return make_shared<FutureTensor>(*Gamma34, task18);
 }
 
 shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma37_() {

commit f73b90c339c676b502899913ec8aaca8e5894a01

bug re-introduced by d96742e09cfc5a21e94dfaac075a9e681588299b

src/smith/caspt2/CASPT2_gamma.cc
@@ -31,7 +31,7 @@
 using namespace std;
 using namespace bagel;
 using namespace bagel::SMITH;
-using namespace bagel::SMITH::CASPT2;
+using bagel::SMITH::CASPT2::FutureTensor;
 
 shared_ptr<FutureTensor> CASPT2::CASPT2::Gamma0_() {
   vector<IndexRange> Gamma0_index = {active_, active_, active_, active_};