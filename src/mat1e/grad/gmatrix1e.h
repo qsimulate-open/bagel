716c9325f4958ac6de8ae657bef728d1df2e54bd
Nils Strand <n-strand@compute-0-6.local>
2017-10-15 00:01:39 -0500

    added gradient matrix classes

---
+++ src/mat1e/grad/gmatrix1e.h
@@ -0,0 +1,90 @@
+//
+// BAGEL - Brilliantly Advanced General Electronic Structure Library
+// Filename: gmatrix1e.h
+// Copyright (C) 2017 Toru Shiozaki
+//
+// Author: Nils Strand <nilsstrand2022@u.northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#ifndef __SRC_MAT1E_GRAD_GMATRIX1E_H
+#define __SRC_MAT1E_GRAD_GMATRIX1E_H
+
+#include <src/mat1e/matrix1e.h>
+
+namespace bagel {
+
+template <typename MatType> class GMatrix1eTask;
+
+// specialized matrix for gradient 1e integrals
+template <typename MatType = Matrix>
+class GMatrix1e {
+  friend class GMatrix1eTask<MatType>;
+  protected:
+    std::vector<std::shared_ptr<MatType>> matrices_;
+
+    virtual void init(std::shared_ptr<const Molecule>);
+    virtual void computebatch(const std::array<std::shared_ptr<const Shell>,2>&, const std::vector<int>&, const std::vector<int>&, std::shared_ptr<const Molecule>) = 0;
+
+  public:
+    GMatrix1e() { }
+    GMatrix1e(std::shared_ptr<const Molecule>);
+    GMatrix1e(const GMatrix1e&);
+    virtual ~GMatrix1e() { }
+
+    void ax_plus_y(const double a, const GMatrix1e<MatType>& o) {
+      std::transform(o.matrices_.begin(), o.matrices_.end(), matrices_.begin(), matrices_.begin(),
+                     [&a](std::shared_ptr<MatType> p, std::shared_ptr<MatType> q) { q->ax_plus_y(a, p); return q; });
+    }
+
+    std::shared_ptr<MatType>& data(const int i) { return matrices_[i]; }
+    std::shared_ptr<const MatType> data(const int i) const { return matrices_[i]; }
+
+    MatType& operator[](const int i) { return *matrices_[i]; }
+    const MatType& operator[](const int i) const { return *matrices_[i]; }
+
+    void fill_upper()       { for (auto& i : matrices_) i->fill_upper(); }
+    void fill_upper_conjg() { for (auto& i : matrices_) i->fill_upper_conjg(); }
+    template<typename DataType>
+    void scale(const DataType a) { for (auto& i : matrices_) i->scale(a); }
+
+    size_t size() const { return matrices_.size(); }
+
+    virtual void print(const std::string name = "", const int len = 10) const;
+
+};
+
+template <typename MatType = Matrix>
+class GMatrix1eTask {
+  protected:
+    GMatrix1e<MatType>* parent_;
+    std::array<std::shared_ptr<const Shell>, 2> shell_;
+    std::array<int,4> atomindex_;
+    std::array<int,4> offset_;
+    std::shared_ptr<const Molecule> mol_;
+  public:
+    GMatrix1eTask<MatType>(const std::array<std::shared_ptr<const Shell>,2>& s, const std::vector<int>& a, const std::vector<int>& o, std::shared_ptr<const Molecule> m, GMatrix1e<MatType>* p)
+      : parent_(p), shell_(s), atomindex_(a), offset_(o), mol_(m) { }
+
+    void compute() const { parent_->computebatch(shell_, atomindex_, offset_, mol_); }
+};
+
+}
+
+#endif
---
+++ src/mat1e/grad/goverlap.cc
@@ -0,0 +1,59 @@
+//
+// BAGEL - Brilliantly Advanced General Electronic Structure Library
+// Filename: goverlap.cc
+// Copyright (C) 2017 Toru Shiozaki
+//
+// Author: Nils Strand <nilsstrand2022@u.northwestern.edu>
+// Maintainer: Shiozaki group
+//
+// This file is part of the BAGEL package.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#include <src/mat1e/grad/goverlap.h>
+#include <src/integral/os/goverlapbatch.h>
+
+using namespace std;
+using namespace bagel;
+
+GOverlap::GOverlap(shared_ptr<const Molecule> mol) : GMatrix1e(mol) {
+
+	init(mol);
+	// fill_upper();
+
+}
+
+
+void GOverlap::computebatch(const array<shared_ptr<const Shell>,2>& s, const vector<int>& a, const vector<int>& o, shared_ptr<const Molecule> m) {
+
+	// s = [b1, b0]
+	assert(s.size() == 2);
+
+	GOverlapBatch batch(s);
+	batch.compute();
+
+	const int dimb1 = s[0]->nbasis(), dimb0 = s[1]->nbasis();
+	const int iatom0 = a[0], iatom1 = a[1], natom = m->natom();
+	const int offsetb0 = o[0], offsetb1 = o[1];
+	const int jatom0 = batch.swap01() ? iatom1 : iatom0, jatom1 = batch.swap01() ? iatom0 : iatom1;
+
+	for (int k = 0; k != 3; ++k) {
+		for (int i = offsetb0, j = 0; i != offsetb0 + dimb0 ; ++i, ++j) {
+			blas::ax_plus_y_n(1, batch.data() + k * batch.size_block() + j * dimb1, dimb1, matrices_[jatom1]->element_ptr(offsetb1, i));
+			blas::ax_plus_y_n(1, batch.data() + (k + 3) * batch.size_block() + j * dimb1, dimb1, matrices_[jatom0]->element_ptr(offsetb1, i));
+		}
+	}
+}
