f7d483bb057e449b287d630b5c1f39d9c7263625
Raymond Wang <raymondwang@u.northwestern.edu>
2017-12-07 21:20:49 -0600

    get rid of some debug code to get ready for merging

--- src/asd/dmrg/asd_dmrg_compute.cc
+++ src/asd/dmrg/asd_dmrg_compute.cc
@@ -95,16 +95,12 @@ void ASD_DMRG::compute() {
       auto mnmx = minmax_element(sweep_energies_[i].begin(), sweep_energies_[i].end());
       const double sweep_average = accumulate(sweep_energies_[i].begin(), sweep_energies_[i].end(), 0.0)/static_cast<double>(sweep_energies_[i].size());
       const double sweep_range = *mnmx.second - *mnmx.first;
-      cout << "sweep energy : " << endl;
-      for (auto i : sweep_energies_[i])
-        cout << setprecision(12) << i << endl;
-      cout << "end of print" << endl;
 
       if (iter != 0)
-        cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(12) << sweep_average << setw(12) << setprecision(8) << sweep_range
+        cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(8) << sweep_average << setw(12) << setprecision(8) << sweep_range
                                                                                << setw(12) << setprecision(8) << energies_[i] - sweep_average << endl;
       else
-        cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(12) << sweep_average << setw(12) << setprecision(8) << sweep_range
+        cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(8) << sweep_average << setw(12) << setprecision(8) << sweep_range
                                                                                << setw(12) << "---------" << endl;
 
       conv &= abs(energies_[i] - sweep_average) < thresh_;
@@ -193,16 +189,12 @@ void ASD_DMRG::down_sweep() {
         auto mnmx = minmax_element(sweep_energies_[i].begin(), sweep_energies_[i].end());
         const double sweep_average = accumulate(sweep_energies_[i].begin(), sweep_energies_[i].end(), 0.0)/static_cast<double>(sweep_energies_[i].size());
         const double sweep_range = *mnmx.second - *mnmx.first;
-        cout << "sweep energy : " << endl;
-        for (auto i : sweep_energies_[i])
-          cout << setprecision(12) << i << endl;
-        cout << "end of print" << endl;
 
         if (iter != 0)
-          cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(12) << sweep_average << setw(12) << setprecision(8) << sweep_range
+          cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(8) << sweep_average << setw(12) << setprecision(8) << sweep_range
                                                                                   << setw(12) << setprecision(8) << energies[i] - sweep_average << endl;
         else
-          cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(12) << sweep_average << setw(12) << setprecision(8) << sweep_range
+          cout << setw(6) << iter << setw(6) << i << setw(18) << setprecision(8) << sweep_average << setw(12) << setprecision(8) << sweep_range
                                                                                   << setw(12) << "---------" << endl;
 
         conv &= abs(energies[i]-sweep_average) < down_thresh_;
