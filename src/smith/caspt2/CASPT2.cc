9ea8d7cd7dfc1be0b0052917d94a8e69ee74dc28
Jae Woo Park <triplepjw@gmail.com>
2018-02-09 15:32:44 -0600

    a different way of correct for shift. temporarily removed reference weight.

--- src/smith/caspt2/CASPT2.cc
+++ src/smith/caspt2/CASPT2.cc
@@ -169,27 +169,30 @@ void CASPT2::CASPT2::solve() {
   timer.tick_print("CASPT2 energy evaluation");
   cout << endl;
 
+  // recalculate the energy without the shift
   for (int istate = 0; istate != nstates_; ++istate) {
-    n = init_residual();
-    double norm = 0.0;
-    for (int jst = 0; jst != nstates_; ++jst) { // bra
-      for (int ist = 0; ist != nstates_; ++ist) { // ket
+    rall_[istate]->zero();
+    for (int jst = 0; jst != nstates_; ++jst) { // jst bra vector
+      for (int ist = 0; ist != nstates_; ++ist) { // ist ket vector
         if (info_->sssr() && (jst != istate || ist != istate))
           continue;
         set_rdm(jst, ist);
         t2 = t2all_[istate]->at(ist);
-        shared_ptr<Queue> normq = make_normq(true, jst == ist);
-        while (!normq->done())
-          normq->next_compute();
-        norm += dot_product_transpose(n, t2all_[istate]->at(jst));
+        r = rall_[istate]->at(jst);
+
+        e0_ = e0all_[istate];
+        shared_ptr<Queue> queue = make_residualq(true, jst == ist);
+        while (!queue->done())
+          queue->next_compute();
+        diagonal(r, t2, jst == ist);
       }
     }
-
-    pt2energy_[istate] = energy_[istate]+(*eref_)(istate,istate) - info_->shift()*norm;
+    pt2energy_[istate] = (*eref_)(istate,istate) + detail::real(dot_product_transpose(sall_[istate], t2all_[istate])) * 2.0;
+    pt2energy_[istate] += detail::real(dot_product_transpose(rall_[istate], t2all_[istate]));
     cout << "    * CASPT2 energy : state " << setw(2) << istate << fixed << setw(20) << setprecision(10) << pt2energy_[istate] << endl;
     if (info_->shift() != 0.0)
       cout << "        w/o shift correction  " << fixed << setw(20) << setprecision(10) << energy_[istate]+(*eref_)(istate,istate) << endl;
-    cout << "        reference weight      " << fixed << setw(20) << setprecision(10) << 1.0/(1.0+norm) << endl;
+    // TODO reference weight is not printed here
     cout << endl;
   }
 
