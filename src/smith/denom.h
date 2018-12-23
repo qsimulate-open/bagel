ab01ab0314da49d44a08060506be516889644375
Jae Woo Park <triplepjw@gmail.com>
2018-12-23 20:28:14 +0900

    not needed

--- src/smith/denom.h
+++ src/smith/denom.h
@@ -60,7 +60,6 @@ class Denom {
     virtual void compute() = 0;
 
     virtual const ViewType shalf(const std::string, const int) const = 0;
-    virtual VectorB denom(const std::string& tag, const int ist) const = 0;
     virtual double denom(const std::string& tag, const int ist, const size_t i) const = 0;
 };
 
@@ -94,7 +93,6 @@ class Denom_SSSR : public Denom<DataType> {
 
     const ViewType shalf(const std::string tag, const int ist) const { return *(shalf_.at(tag)[ist]); }
     void compute() override; 
-    VectorB denom(const std::string& tag, const int ist) const override { return denom_.at(tag)[ist]; }
     double denom(const std::string& tag, const int ist, const size_t i) const override { return denom_.at(tag)[ist](i); }
 }; 
 
@@ -130,7 +128,6 @@ class Denom_MSMR : public Denom<DataType> {
       return shalf_.at(tag)->slice(ist*dim, (ist+1)*dim);
     }
     void compute() override; 
-    VectorB denom(const std::string& tag, const int ist) const override { return denom_.at(tag); }
     double denom(const std::string& tag, const int ist, const size_t i) const override { return denom_.at(tag)(i); }
 }; 
 
