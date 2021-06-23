293b72c047e3f2c9faeee7a9ed397ad4139b0269
Susi Lehtola <susi.lehtola@gmail.com>
2021-06-22 23:46:53 -0400

    Another missing boost include

--- src/util/math/btas_interface.cc
+++ src/util/math/btas_interface.cc
@@ -22,6 +22,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+#include <boost/archive/basic_archive.hpp>
 #include <src/util/serialization.h>
 #include <src/util/math/btas_interface.h>
 
